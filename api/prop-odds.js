14c4ed9e7a1cf05e2e4209207ee5f40b197c258a
Bkhamilton <bkhamilton310@gmail.com>
2024-03-30 15:17:52 -0400

    Added function for getGameProps,getGameMarketProps

--- api/prop-odds.js
+++ api/prop-odds.js
@@ -1,4 +1,4 @@
-// Function to fetch data from API and return it
+// Function to fetch game data from prop-odds api based on sport
 export const getGames = async (sport) => {
     try {
       const today = new Date();
@@ -9,4 +9,26 @@ export const getGames = async (sport) => {
     } catch (error) {
       console.error(error);
     }
+};
+
+// Function to fetch game props from prop-odds api based on gameId
+export const getGameProps = async (gameId) => {
+  try {
+    const response = await fetch(`https://api.prop-odds.com/beta/markets/${gameId}?api_key=${process.env.PROP_ODDS_API_KEY}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Function to fetch game market props from prop-odds api based on gameId and market
+export const getGameMarketProps = async (gameId, market) => {
+  try {
+    const response = await fetch(`https://api.prop-odds.com/beta/odds/${gameId}/${market}?api_key=${process.env.PROP_ODDS_API_KEY}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
