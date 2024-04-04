d7b2d3e9ac4b5b7179a620821dbffc726eb871f2
Bkhamilton <bkhamilton310@gmail.com>
2024-04-04 18:39:07 -0400

    moved exampleBet to new file

---
+++ data/exampleBetData.js
@@ -0,0 +1,104 @@
+export const myBetList = [
+    {
+      date: '4/4/24',
+      odds: '+543',
+      bets: [
+        {
+          date: '4/4/24',
+          sport: 'NBA',
+          home: 'LAL',
+          away: 'DEN',
+          odds: '+168',
+          legs: [
+            {
+              betTarget: 'Lebron James',
+              stat: 'Points',
+              alt: true,
+              line: 20,
+              overUnder: 'over'
+            },
+            {
+              betTarget: 'Lebron James',
+              stat: 'Assists',
+              alt: false,
+              line: 5.5,
+              overUnder: 'over'
+            },
+            {
+              betTarget: 'Nikola Jokic',
+              stat: 'Points',
+              alt: false,
+              line: 26.5,
+              overUnder: 'over'
+            }   
+          ]
+        },
+        {
+          date: '4/4/24',
+          sport: 'NBA',
+          home: 'HOU',
+          away: 'PHX',
+          odds: '+140',
+          legs: [
+            {
+              betTarget: 'Alperen Sengun',
+              stat: 'Points',
+              alt: true,
+              line: 18,
+              overUnder: 'over'
+            },
+            {
+              betTarget: 'Alperen Sengun',
+              stat: 'Assists',
+              alt: false,
+              line: 3.5,
+              overUnder: 'over'
+            },
+            {
+              betTarget: 'Fred VanVleet',
+              stat: 'Threes',
+              alt: true,
+              line: 2,
+              overUnder: 'over'
+            }   
+          ]
+        }, 
+      ]
+    },
+    {
+      date: '4/4/24',
+      odds: '+200',
+      bets: [
+        {
+          date: '4/4/24',
+          sport: 'NBA',
+          home: 'BOS',
+          away: 'MIA',
+          odds: '+120',
+          legs: [
+            {
+              betTarget: 'Jayson Tatum',
+              stat: 'Points',
+              alt: true,
+              line: 25,
+              overUnder: 'over'
+            },
+            {
+              betTarget: 'Jayson Tatum',
+              stat: 'Rebounds',
+              alt: false,
+              line: 7.5,
+              overUnder: 'over'
+            },
+            {
+              betTarget: 'Bam Adebayo',
+              stat: 'Assists',
+              alt: false,
+              line: 5.5,
+              overUnder: 'over'
+            }   
+          ]
+        }, 
+      ]
+    }
+  ];
