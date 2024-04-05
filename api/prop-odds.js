6b1fb8e2fa3d94eb03de18050e91c460730b53ca
Bkhamilton <bkhamilton310@gmail.com>
2024-04-05 16:55:51 -0400

    Added functions to get Sports Data

--- api/prop-odds.js
+++ api/prop-odds.js
@@ -1,5 +1,6 @@
 // Function to fetch game data from prop-odds api based on sport
 import secrets from "../secrets";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const getGames = async (sport) => {
     try {
@@ -31,6 +32,40 @@ export const getGameMarketProps = async (gameId, market) => {
     const data = await response.json();
     return data;
   } catch (error) {
+    console.error(error);
+  }
+};
+
+// Function to fetch data from API and store it in AsyncStorage
+export const fetchData = async (sports) => {
+    try {
+      for (let sport of sports) {
+        const data = await getGames(sport);
+        await AsyncStorage.setItem(`${sport}Data`, JSON.stringify(data));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+// Function to retrieve data from AsyncStorage
+export const retrieveData = async (sports) => {
+  try {
+    let data = [];
+    for (let sport of sports) {
+      const value = await AsyncStorage.getItem(`${sport}Data`);
+      if (value !== null) {
+        // We have data!!
+        data.push({ sport, data: JSON.parse(value) });
+      } else {
+        // No data in AsyncStorage, fetch from API
+        const fetchedData = await fetchData([sport]);
+        data.push({ sport, data: fetchedData });
+      }
+    }
+    return data;
+  } catch (error) {
+    // Error retrieving data
     console.error(error);
   }
 };
