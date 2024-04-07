af9d3b3272d4470f94858a4e7d9eff358eec3e86
Bkhamilton <bkhamilton310@gmail.com>
2024-04-07 18:11:38 -0400

    Add TodaysBets component

--- components/Home/BetView/TodaysBets.js
+++ components/Home/BetView/TodaysBets.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { TouchableOpacity, Text, View } from '../Themed';
+import { TouchableOpacity, Text, View } from '../../Themed';
 
 import Colors from '@/constants/Colors';
 
