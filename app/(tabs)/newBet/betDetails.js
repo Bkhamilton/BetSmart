fa1a4a33dc08ef1d96f5f400a89182e243e845a0
Bkhamilton <bkhamilton310@gmail.com>
2024-04-11 16:44:28 -0400

    Refactor BetDetailsScreen component to use separate GameDetails and NoGameDetails components

--- app/(tabs)/newBet/betDetails.js
+++ app/(tabs)/newBet/betDetails.js
@@ -2,19 +2,14 @@ import { StyleSheet, Dimensions } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Text, View, TouchableOpacity } from '@/components/Themed';
 import Header from '@/components/Header/Header';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function BetDetailsScreen() {
-
-  const router = useRouter();
-
-  const handleClose = () => {
-    router.navigate('newBet/selectGame');
-  };
-
+function GameDetails({ game, handleClose }) {
   return (
     <View style={styles.container}>
-      <Header title={'Bet Details'}/>
+      <View style={styles.headerContainer}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{game.league}</Text>
+      </View>
       <View>
         <Text>Bet Details</Text>
         <TouchableOpacity onPress={handleClose}>
@@ -25,23 +20,50 @@ export default function BetDetailsScreen() {
   );
 }
 
+function NoGameDetails({ handleClose }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>League</Text>
+      </View>
+      <View>
+        <Text>No Game Selected</Text>
+        <TouchableOpacity onPress={handleClose}>
+          <Text>Close</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default function BetDetailsScreen({ game }) {
+
+  const [newGame, setNewGame] = useState({ away_team: 'Team 1', home_team: 'Team 2' });
+
+  useEffect(() => {
+    if (game) {
+      setNewGame(game);
+    }
+  }, [game]);
+
+  const router = useRouter();
+
+  const handleClose = () => {
+    router.navigate('newBet/selectGame');
+  };
+
+  return game ? <GameDetails game={game} handleClose={handleClose} /> : <NoGameDetails handleClose={handleClose} />;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  chartContainer: {
-    position: 'relative',
-    justifyContent: 'center',
+  headerContainer: {
+    height: 84, 
+    paddingHorizontal: 20, 
+    paddingTop: 48,
+    borderBottomWidth: 1,
     alignItems: 'center',
-  },
-  profitText: {
-    position: 'absolute',
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: 'white',
   },
 });
