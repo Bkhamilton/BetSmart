5939b72c5e1868b7e4632458a5922d0f6b58fdbb
Bkhamilton <bkhamilton310@gmail.com>
2024-04-11 17:21:01 -0400

    Refactor BetDetailsScreen component to include separate GameDetails and NoGameDetails components

--- app/(tabs)/newBet/betDetails.js
+++ app/(tabs)/newBet/betDetails.js
@@ -2,41 +2,56 @@ import { StyleSheet, Dimensions } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Text, View, TouchableOpacity } from '@/components/Themed';
 import Header from '@/components/Header/Header';
+import { Ionicons } from '@expo/vector-icons';
 import React, { useState, useEffect } from 'react';
 
-function GameDetails({ game, handleClose }) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{game.league}</Text>
-      </View>
-      <View>
-        <Text>Bet Details</Text>
-        <TouchableOpacity onPress={handleClose}>
-          <Text>Close</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-function NoGameDetails({ handleClose }) {
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>League</Text>
-      </View>
-      <View>
-        <Text>No Game Selected</Text>
-        <TouchableOpacity onPress={handleClose}>
-          <Text>Close</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
 export default function BetDetailsScreen({ game }) {
+
+  function GameDetails({ game, handleClose }) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={{ flex: 0.2, alignItems: 'center' }}>
+            <TouchableOpacity onPress={handleClose}>
+              <Ionicons name="return-up-back" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 0.6, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{game.league}</Text>
+          </View>
+          <View style={{ flex: 0.2, alignItems: 'center' }}>
+            
+          </View>                
+        </View>
+        <View>
+          <Text>Bet Details</Text>
+        </View>
+      </View>
+    );
+  }
+  
+  function NoGameDetails({ handleClose }) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={{ flex: 0.2 }}>
+            <TouchableOpacity onPress={handleClose}>
+              <Ionicons name="return-up-back" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 0.6, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>League</Text>
+          </View>
+          <View style={{ flex: 0.2, alignItems: 'center' }}>
+            
+          </View>                
+        </View>
+        <View>
+          <Text>No Game Selected</Text>
+        </View>
+      </View>
+    );
+  }
 
   const [newGame, setNewGame] = useState({ away_team: 'Team 1', home_team: 'Team 2' });
 
@@ -64,6 +79,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20, 
     paddingTop: 48,
     borderBottomWidth: 1,
-    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
