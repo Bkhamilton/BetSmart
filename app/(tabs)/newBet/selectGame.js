94018e2dd5c1ee8f182894a8913e01905ec3433b
Bkhamilton <bkhamilton310@gmail.com>
2024-05-01 21:02:54 -0400

    Cleaned Up, Updated Bank Button

--- app/(tabs)/newBet/selectGame.js
+++ app/(tabs)/newBet/selectGame.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback, useContext } from 'react';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, Image } from 'react-native';
 import { Text, View, TouchableOpacity, Pressable } from '@/components/Themed';
 import { sportsData, nbaTeams, nflTeams, mlbTeams, nhlTeams, nbaGamesToday } from '@/data/exampleTeamData';
 import MainButtons from '@/components/PlaceBet/MainButtons';
@@ -11,6 +11,8 @@ import GameListSlider from '@/components/PlaceBet/GameListSlider.js';
 import SportSlider from '@/components/PlaceBet/SportsSlider.js';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { BetContext } from '@/contexts/BetContext';
+import draftkings from '@/assets/images/DraftKings.png';
+import useTheme from '@/hooks/useTheme';
 
 export default function SelectGameScreen() {
   const { currentGame, setCurrentGame } = useContext(BetContext);
@@ -71,45 +73,40 @@ export default function SelectGameScreen() {
   const curSportData = allSportsData.find(sportData => sportData.sport.toLowerCase() === curSport.title.toLowerCase());
   const curSportGames = curSportData ? curSportData.data : [];
 
-  return (
-    <View style={styles.container}>
+  const { iconColor, mainGreen } = useTheme();
+
+  const SelectGameHeader = () => {
+    return (
       <View style={styles.headerContainer}>
-        <View style={{ flex: 0.2 }}>
-          <Text style={{ fontSize: 24, fontWeight: '500' }}>$200</Text>
+        <View style={{ flex: 0.3 }}>
+          
         </View>
-        <View style={{ flex: 0.6, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 0.4, alignItems: 'center', justifyContent: 'center' }}>
           <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{header}</Text>
         </View>
-        <View style={{ flex: 0.2, alignItems: 'flex-end' }}>
-          <TouchableOpacity>
-            <FontAwesome5 name='wallet' size={24} color={'black'} />
-          </TouchableOpacity>
+        <View style={{ flex: 0.3, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
+          <Pressable style={[styles.bankButton, { backgroundColor: mainGreen, borderColor: mainGreen }] }>
+            <Text style={{ fontSize: 20, fontWeight: '500', marginRight: 8 }}>$200</Text>
+            <Image source={draftkings} style={{ width: 32, height: 32, borderRadius: 8 }} />
+          </Pressable>
         </View>
       </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <SelectGameHeader />
       <View style={{ flex: 1, alignItems: 'center' }}>
-        <View style={{ flexDirection: 'row', paddingTop: 16, width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Choose {curCategory}</Text>
-        </View>
-        { sportSelected && 
-          <View style={{ paddingVertical: 4  }}>
-            <SportSlider sports={sportsData} selectSport={selectSport} curSport={curSport}/>
-          </View>
-        }
         { sportSelected &&
-          <View style={{ alignItems: 'center' }}>
-            {
-              curGame.home_team.length > 0 &&
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 20, }}>
-                  <View style={{ flex: 1, borderWidth: 1, paddingHorizontal: 12, paddingVertical: 8, marginHorizontal: 8 }}>
-                    <Text style={{ textAlign: 'center' }}>{curHomeTeam.team}</Text>
-                  </View>
-                  <View style={{ flex: 1, borderWidth: 1, paddingHorizontal: 12, paddingVertical: 8, marginHorizontal: 8 }}>
-                    <Text style={{ textAlign: 'center' }}>{curAwayTeam.team}</Text>
-                  </View>
-                </View>
-            }
-            { !gameSelected && <GameList games={curSportGames.games} selectGame={game => handleSelectGame({ game })} sport={curSportGames.league}/> }
-          </View>
+          <>
+            <View style={{ paddingVertical: 10  }}>
+              <SportSlider sports={sportsData} selectSport={selectSport} curSport={curSport}/>
+            </View> 
+            <View style={{ alignItems: 'center' }}>
+              <GameList games={curSportGames.games} selectGame={game => handleSelectGame({ game })} sport={curSportGames.league}/>
+            </View>
+          </> 
         }
         { !sportSelected &&
           <View style={styles.buttonsContainer}>
@@ -188,11 +185,20 @@ const styles = StyleSheet.create({
   },
   headerContainer: {
     height: 84, 
-    paddingHorizontal: 20, 
+    paddingHorizontal: 10, 
     paddingTop: 48,
     borderBottomWidth: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  bankButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: -8,
+    paddingLeft: 8,
+  }
 });
