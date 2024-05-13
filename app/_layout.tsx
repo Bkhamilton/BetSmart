75d9ced45ed7e984d552fdf945e79c802ef623fd
Bkhamilton <bkhamilton310@gmail.com>
2024-05-13 13:49:11 -0400

    added Suspense to Imports

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { SQLiteProvider } from 'expo-sqlite';
 import { initializeDatabase } from '@/api/sqlite';
 
