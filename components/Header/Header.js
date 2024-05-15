08da79e65ea8e1a27830d4ac3179900d2c77ece9
Bkhamilton <bkhamilton310@gmail.com>
2024-05-14 21:42:33 -0400

    removed unneeded imports

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-
-import { ExternalLink } from '../ExternalLink';
-import { MonoText } from '../StyledText';
 import { Text, View } from '../Themed';
 
 export default function Header({ title, children }) {
