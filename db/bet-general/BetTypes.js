d196a7b81db34ada7223ea3cef45cd3a51ff67d7
Ben Hamilton <43786988+Bkhamilton@users.noreply.github.com>
2024-05-16 08:09:46 -0400

    Update BetTypes.js

--- db/bet-general/BetTypes.js
+++ db/bet-general/BetTypes.js
@@ -1,1 +1,29 @@
+import * as SQLite from 'expo-sqlite';
 
+// Function to get all bet types
+export const getAllBetTypes = async (db) => {
+  const allRows = await db.getAllAsync('SELECT * FROM BetTypes');
+  return allRows;
+};
+
+// Function to get a bet type
+export const getBetType = async (db, betTypeId) => {
+  const betType = await db.getAsync('SELECT * FROM BetTypes WHERE id = ?', [betTypeId]);
+  return betType;
+};
+
+// Function to insert a bet type
+export const insertBetType = async (db, betType, description) => {
+  const result = await db.runAsync('INSERT INTO BetTypes (betType, description) VALUES (?, ?)', [betType, description]);
+  return result.lastInsertRowId;
+};
+
+// Function to update a bet type
+export const updateBetType = async (db, betTypeId, betType, description) => {
+  await db.runAsync('UPDATE BetTypes SET betType = ?, description = ? WHERE id = ?', [betType, description, betTypeId]);
+};
+
+// Function to delete a bet type
+export const deleteBetType = async (db, betTypeId) => {
+  await db.runAsync('DELETE FROM BetTypes WHERE id = ?', [betTypeId]);
+};
