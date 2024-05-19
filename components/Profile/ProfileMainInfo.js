d876a92d4514ed9992d0e9c206f32039df3f0af8
Bkhamilton <bkhamilton310@gmail.com>
2024-05-19 18:12:14 -0400

    renamed ProfileHeader to ProfileMainInfo

---
+++ components/Profile/ProfileMainInfo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, View } from '@/components/Themed';
+
+import Colors from '@/constants/Colors';
+
+export default function ProfileMainInfo({ user }) {
+
+    const { id, name, email, username, password } = user;
+
+    return (
+        <View style={styles.container}>
+            <View style={{ justifyContent: 'center' }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+                    <Text>100 Bets</Text>
+                    <Text>$200 All Time</Text>
+                </View>
+                <View style={{ paddingVertical: 4 }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{name}</Text>
+                </View>
+                <Text style={{ fontSize: 16 }}>{username}</Text>
+            </View>
+            <View style={{ paddingVertical: 12 }}>
+                <View style={{ height: 100, width: 100, borderRadius: 50, borderWidth: 1 }}/>
+            </View>
+        </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 12,
+        paddingLeft: 20,
+        paddingVertical: 16,
+    },
+  });
