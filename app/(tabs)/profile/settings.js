d2f36d33485cab510243dc10c9a444624fc0ecf5
Bkhamilton <bkhamilton310@gmail.com>
2024-05-19 18:14:02 -0400

    refactor: Remove unused ProfileHeader component

--- app/(tabs)/profile/settings.js
+++ app/(tabs)/profile/settings.js
@@ -3,7 +3,6 @@ import { useRouter } from 'expo-router';
 import { Text, View, TouchableOpacity, ScrollView } from '@/components/Themed';
 import Header from '@/components/Header/Header';
 import { FontAwesome5 } from '@expo/vector-icons';
-import ProfileHeader from '@/components/Profile/ProfileHeader';
 import AccountInfo from '../../../components/Profile/Settings/AccountInfo';
 import SettingsOptions from '../../../components/Profile/Settings/SettingsOptions';
 
