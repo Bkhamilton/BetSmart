20a6795f421317e40144665f4a5d8a77ca738ac8
Ben Hamilton <43786988+Bkhamilton@users.noreply.github.com>
2024-05-27 09:55:43 -0400

    Update markets.js

--- api/prop-odds/markets.js
+++ api/prop-odds/markets.js
@@ -1,1 +1,7 @@
-hi
+import secrets from "@/secrets";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getGamesByDate, insertGame } from "@/db/general/Games";
+import { getTeamIds } from "@/db/general/Teams";
+import { getCurrentSeason } from "@/db/general/Seasons";
+import { getLeagueByName } from "@/db/general/Leagues";
+import { getTodaysGameswithNames } from "@/db/general/Games";
