6570ee65d32195d911173d3ff270c1325cf5f828
Ben Hamilton <43786988+Bkhamilton@users.noreply.github.com>
2024-05-27 10:03:03 -0400

    Update markets.js

--- api/prop-odds/markets.js
+++ api/prop-odds/markets.js
@@ -5,3 +5,15 @@ import { getTeamIds } from "@/db/general/Teams";
 import { getCurrentSeason } from "@/db/general/Seasons";
 import { getLeagueByName } from "@/db/general/Leagues";
 import { getTodaysGameswithNames } from "@/db/general/Games";
+
+export const getMarkets = async (gameId, market) => {
+    try {
+      const today = new Date();
+      const date = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+      const response = await fetch(`https://api.prop-odds.com/beta/odds/${gameId}/${market}?api_key=%7BYOUR_API_KEY%7D`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+};
