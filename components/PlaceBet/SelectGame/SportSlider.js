bc77f99eb5746fcf0d60bf7d2f8bb83804221b0a
Bkhamilton <bkhamilton310@gmail.com>
2024-05-27 21:38:56 -0400

    removed unused SportList, moved SportSlider to SelectGame folder

--- components/PlaceBet/SelectGame/SportSlider.js
+++ components/PlaceBet/SelectGame/SportSlider.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import { StyleSheet, FlatList, Image } from 'react-native';
-import { Text, View, TouchableOpacity } from '../Themed';
+import { Text, View, TouchableOpacity } from '@/components/Themed';
 import useTheme from '@/hooks/useTheme';
 
-import Colors from '@/constants/Colors';
-
-import mlb from '@/assets/images/mlb.png';
-import nba from '@/assets/images/nba.png';
-import nhl from '@/assets/images/nhl.png';
-import nfl from '@/assets/images/nfl.png';
-import mls from '@/assets/images/mls.png';
-import ncaa from '@/assets/images/ncaa.png';
-import pga from '@/assets/images/pga.png';
-import ufc from '@/assets/images/ufc.png';
-
-export default function SportSlider({ sports, selectSport, curSport, leagues }) {
+export default function SportSlider({ sports, selectSport, curSport }) {
 
     const { mainBlue, text, borderColor, iconColor } = useTheme();
-
-    const nameToIcon = {
-        'NBA': nba,
-        'NHL': nhl,
-        'MLB': mlb,
-        'NFL': nfl,
-        'MLS': mls,
-        'NCAAB': ncaa,
-        'PGA': pga,
-        'UFC': ufc,
-    }
 
     return (
         <View style={{ height: 78, backgroundColor: 'transparent' }}>
