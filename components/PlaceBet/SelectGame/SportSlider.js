118142ef34f6a1aebe4c135ad169757f0ac31370
Bkhamilton <bkhamilton310@gmail.com>
2024-05-27 22:19:21 -0400

    added icon imports

--- components/PlaceBet/SelectGame/SportSlider.js
+++ components/PlaceBet/SelectGame/SportSlider.js
@@ -3,7 +3,27 @@ import { StyleSheet, FlatList, Image } from 'react-native';
 import { Text, View, TouchableOpacity } from '@/components/Themed';
 import useTheme from '@/hooks/useTheme';
 
+import mlb from '@/assets/images/mlb.png';
+import nba from '@/assets/images/nba.png';
+import nhl from '@/assets/images/nhl.png';
+import nfl from '@/assets/images/nfl.png';
+import mls from '@/assets/images/mls.png';
+import ncaa from '@/assets/images/ncaa.png';
+import pga from '@/assets/images/pga.png';
+import ufc from '@/assets/images/ufc.png';
+
 export default function SportSlider({ sports, selectSport, curSport }) {
+
+    const nameToIcon = {
+        'NBA': nba,
+        'NHL': nhl,
+        'MLB': mlb,
+        'NFL': nfl,
+        'MLS': mls,
+        'NCAAB': ncaa,
+        'PGA': pga,
+        'UFC': ufc,
+    }
 
     const { mainBlue, text, borderColor, iconColor } = useTheme();
 
