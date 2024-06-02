c4fd3f9cc8a077f1149cad980d9d3b287bb7691d
Bkhamilton <bkhamilton310@gmail.com>
2024-06-02 17:22:42 -0400

    feat: Add CategoryInfo component for displaying category information in BetDetails screen

---
+++ components/PlaceBet/BetDetails/CategoryInfo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, View } from '@/components/Themed';
+import Colors from '@/constants/Colors';
+
+export default function CategoryInfo({ curCategory }) {
+
+    return (
+        <View>
+            {/* Main Block */}
+            <View>
+
+            </View>
+            {/* Alt Category Block */}
+            <View>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    borderWidth: 1,
+    paddingVertical: 12,
+    borderRadius: 8,
+  },
+});
