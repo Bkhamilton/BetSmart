d909fc3fb2a639a8127ff291076ce7d70c7ed7a1
Bkhamilton <bkhamilton310@gmail.com>
2024-06-02 17:22:09 -0400

    chore: Add league category data for NBA, NFL, MLB, and NHL

---
+++ data/leagueCategoryData.js
@@ -0,0 +1,139 @@
+export const NBAcategories = [
+  {
+    title: 'Popular',
+    id: 1,
+  },
+  {
+    title: 'Main',
+    id: 2,
+  },
+  {
+    title: 'Alt Main',
+    id: 3,
+  },
+  {
+    title: 'Player Points',
+    id: 4,
+  },
+  {
+    title: 'Player Rebounds',
+    id: 5,
+  },
+  {
+    title: 'Player Assists',
+    id: 6,
+  },
+  {
+    title: 'Player Threes',
+    id: 7,
+  },
+  {
+    title: 'Player Combos',
+    id: 8,
+  },
+  {
+    title: 'Player Defense',
+    id: 9,
+  },
+  {
+    title: 'Stat Leaders',
+    id: 10,
+  },
+  {
+    title: 'Half',
+    id: 11,
+  },
+  {
+    title: 'Quarters',
+    id: 12,
+  },
+  {
+    title: 'Team Props',
+    id: 13,
+  },
+  {
+    title: 'Game Props',
+    id: 14,
+  },
+]
+
+export const NFLcategories = [
+  {
+    title: 'Popular',
+    id: 1,
+  },
+  {
+    title: 'Main',
+    id: 2,
+  },
+  {
+    title: 'Alt Main',
+    id: 3,
+  },
+  {
+    title: 'Player Props',
+    id: 4,
+  },
+  {
+    title: 'Team Props',
+    id: 5,
+  },
+  {
+    title: 'Game Props',
+    id: 6,
+  },
+]
+
+export const MLBcategories = [
+  {
+    title: 'Popular',
+    id: 1,
+  },
+  {
+    title: 'Main',
+    id: 2,
+  },
+  {
+    title: 'Alt Main',
+    id: 3,
+  },
+  {
+    title: 'Player Props',
+    id: 4,
+  },
+  {
+    title: 'Team Props',
+    id: 5,
+  },
+  {
+    title: 'Game Props',
+    id: 6,
+  },
+]
+
+export const NHLcategories = [
+  {
+    title: 'Popular',
+    id: 1,
+  },
+  {
+    title: 'Main',
+    id: 2,
+  },
+  {
+    title: 'Alt Main',
+    id: 3,
+  },
+  {
+    title: 'Player Props',
+    id: 4,
+  },
+  {
+    title: 'Team Props',
+    id: 5,
+  },
+  {
+    title: 'Game Props',
+    id: 6,
+  },
+]
