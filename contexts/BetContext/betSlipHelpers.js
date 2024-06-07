4ca6a6130580e7849b54f1d09a96c23bbf6a9f0a
Bkhamilton <bkhamilton310@gmail.com>
2024-06-06 21:54:21 -0400

    refactor: Update createLeg function to include odds parameter

--- contexts/BetContext/betSlipHelpers.js
+++ contexts/BetContext/betSlipHelpers.js
@@ -1,33 +1,32 @@
 export function createBetSlip(id, type, date, odds, betAmount, winnings, bets) {
-    return {
-      id,
-      type,
-      date,
-      odds, 
-      betAmount,
-      winnings,
-      bets
-    };
-  }
-  
-  export function createBet(date, sport, home, away, odds, legs) {
-    return {
-      date,
-      sport,
-      home,
-      away,
-      odds,
-      legs
-    };
-  }
-  
-  export function createLeg(type, betTarget, stat, alt, line, overUnder) {
-    return {
-      type,
-      betTarget,
-      stat,
-      alt,
-      line,
-      overUnder
-    };
-  }
+  return {
+    id,
+    type,
+    date,
+    odds, 
+    betAmount,
+    winnings,
+    bets
+  };
+}
+
+export function createBet(date, sport, home, away, odds, legs) {
+  return {
+    date,
+    sport,
+    home,
+    away,
+    odds,
+    legs
+  };
+}
+
+export function createLeg(type, betTarget, stat, line, odds) {
+  return {
+    type,
+    betTarget,
+    stat,
+    line,
+    odds,
+  };
+}
