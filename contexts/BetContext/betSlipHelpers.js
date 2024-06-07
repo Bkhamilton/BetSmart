109d2c47f7b1055f2663d1f2f9b1455ee821f366
Bkhamilton <bkhamilton310@gmail.com>
2024-06-07 15:26:38 -0400

    refactor: Update createBet function parameter name from 'sport' to 'league'

--- contexts/BetContext/betSlipHelpers.js
+++ contexts/BetContext/betSlipHelpers.js
@@ -10,10 +10,10 @@ export function createBetSlip(id, type, date, odds, betAmount, winnings, bets) {
   };
 }
 
-export function createBet(date, sport, home, away, odds, legs) {
+export function createBet(date, league, home, away, odds, legs) {
   return {
     date,
-    sport,
+    league,
     home,
     away,
     odds,
@@ -29,4 +29,85 @@ export function createLeg(type, betTarget, stat, line, odds) {
     line,
     odds,
   };
+}
+
+function calculateCombinedOdds(oddsArray) {
+
+  // If there is only one odds, return it
+  if (oddsArray.length === 1) {
+    return oddsArray[0];
+  }
+
+  let combinedDecimalOdds = 1;
+
+  // Convert each American odds to decimal odds and multiply them together
+  for (let i = 0; i < oddsArray.length; i++) {
+    const numOdds = Number(oddsArray[i]);
+    const decimalOdds = numOdds > 0 ? (numOdds / 100) + 1 : (100 / Math.abs(numOdds)) + 1;
+    combinedDecimalOdds *= decimalOdds;
+  }
+
+  // Subtract 1 from the combined decimal odds
+  combinedDecimalOdds -= 1;
+
+  // Convert the combined decimal odds back to American odds
+  const combinedOdds = combinedDecimalOdds >= 1 ? (combinedDecimalOdds - 1) * 100 : -100 / (combinedDecimalOdds - 1);
+
+  const americanOdds = combinedOdds >= 1 ? `+${Math.round(combinedOdds)}` : `-${Math.round(combinedOdds)}`;
+
+  return americanOdds;
+}
+
+export function updateBet(bet, leg) {
+  // Find the leg
+  const legToUpdate = bet.legs.find(l => 
+    l.type === leg.type && 
+    l.betTarget === leg.betTarget && 
+    l.stat === leg.stat
+  );
+
+  // If the leg doesn't exist, add a new one
+  if (!legToUpdate) {
+    bet.legs.push(leg);
+    bet.odds = calculateCombinedOdds([bet.odds, leg.odds]);
+  } else {
+    // If the leg does exist, update it
+    legToUpdate.line = leg.line;
+    legToUpdate.odds = leg.odds;
+  }
+
+  return bet;
+}
+
+export function updateBetSlip(betSlip, bet, leg) {
+  // Find the bet
+  let betToUpdate = betSlip.bets.find(b => 
+    b.date === bet.date && 
+    b.sport === bet.sport && 
+    b.home === bet.home && 
+    b.away === bet.away
+  );
+
+  // If the bet doesn't exist, add a new one
+  if (!betToUpdate) {
+    betSlip.bets.push(bet);
+
+    // Update the betSlip type
+    if (betSlip.type === 'Single') {
+      betSlip.type = 'Parlay';
+    }
+  } else {
+    // If the bet does exist, update it using updateBet function
+    betToUpdate = updateBet(betToUpdate, leg);
+
+    // Update the betSlip type
+    if (betSlip.type === 'Single') {
+      betSlip.type = 'Same Game Parlay';
+    }
+  }
+
+  // Update the betSlip odds
+  betSlip.odds = calculateCombinedOdds(betSlip.bets.map(bet => bet.odds));
+
+  return betSlip;
 }
