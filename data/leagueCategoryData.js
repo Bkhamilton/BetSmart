f79cf69d434201ee921731545d54f3553524033a
Bkhamilton <bkhamilton310@gmail.com>
2024-06-15 14:52:31 -0400

    added info for LeaguePropsInfo Table

--- data/leagueCategoryData.js
+++ data/leagueCategoryData.js
@@ -1,58 +1,72 @@
 export const NBAcategories = [
   {
     title: 'Popular',
+    info: ['game-lines', 'alt-line'],
     id: 1,
   },
   {
     title: 'Main',
+    info: ['game-lines', 'first-basket', 'player-special', 'team-special'],
     id: 2,
   },
   {
     title: 'Alt Main',
+    info: ['alt-spread', 'alt-total'],
     id: 3,
   },
   {
     title: 'Player Points',
+    info: ['main-line', 'alt-line', 'first-to-reach', 'alt-player-line'],
     id: 4,
   },
   {
     title: 'Player Rebounds',
+    info: ['main-line', 'alt-line', 'first-to-reach', 'alt-player-line'],
     id: 5,
   },
   {
     title: 'Player Assists',
+    info: ['main-line', 'alt-line', 'first-to-reach', 'alt-player-line'],
     id: 6,
   },
   {
     title: 'Player Threes',
+    info: ['main-line', 'alt-line', 'first-to-reach', 'alt-player-line'],
     id: 7,
   },
   {
     title: 'Player Combos',
+    info: ['main-line', 'alt-line'],
     id: 8,
   },
   {
     title: 'Player Defense',
+    info: ['main-line', 'alt-line', 'first-to-reach', 'alt-player-line'],
     id: 9,
   },
   {
     title: 'Stat Leaders',
+    info: ['stat-leaders'],
     id: 10,
   },
   {
     title: 'Half',
+    info: ['half-lines'],
     id: 11,
   },
   {
     title: 'Quarters',
+    info: ['quarter-lines'],
     id: 12,
   },
   {
     title: 'Team Props',
+    info: ['team-props', 'team-special'],
     id: 13,
   },
   {
     title: 'Game Props',
+    info: ['game-props', 'first-to-x', 'alt-game-props'],
     id: 14,
   },
 ]
@@ -60,26 +74,32 @@ export const NBAcategories = [
 export const NFLcategories = [
   {
     title: 'Popular',
+    info: ['game-lines', 'alt-line'],
     id: 1,
   },
   {
     title: 'Main',
+    info: ['game-lines', 'first-to-score', 'team-special'],
     id: 2,
   },
   {
     title: 'Alt Main',
+    info: ['alt-spread', 'alt-total'],
     id: 3,
   },
   {
     title: 'Player Props',
+    info: ['player-props'],
     id: 4,
   },
   {
     title: 'Team Props',
+    info: ['team-props', 'team-special'],
     id: 5,
   },
   {
     title: 'Game Props',
+    info: ['game-props', 'alt-game-props'],
     id: 6,
   },
 ]
@@ -87,30 +107,37 @@ export const NFLcategories = [
 export const MLBcategories = [
   {
     title: 'Popular',
+    info: ['game-lines', 'alt-line'],
     id: 1,
   },
   {
     title: 'Main',
+    info: ['game-lines', 'first-to-score', 'team-special'],
     id: 2,
   },
   {
     title: 'Alt Main',
+    info: ['alt-spread', 'alt-total'],
     id: 3,
   },
   {
     title: 'Batter Props',
+    info: ['hit-props', 'run-props', 'rbi-props', 'total-bases', 'hr-props'],
     id: 4,
   },
   {
     title: 'Pitcher Props',
+    info: ['pitcher-props'],
     id: 5,
   },
   {
     title: 'Innings',
+    info: ['innings-lines', 'first-x-innings'],
     id: 6,
   },
   {
     title: 'Game Props',
+    info: ['game-props', 'alt-game-props'],
     id: 7,
   },
 ]
@@ -118,34 +145,42 @@ export const MLBcategories = [
 export const NHLcategories = [
   {
     title: 'Popular',
+    info: ['game-lines', 'alt-line'],
     id: 1,
   },
   {
     title: 'Main',
+    info: ['game-lines', 'first-goal', 'player-special', 'team-special'],
     id: 2,
   },
   {
     title: 'Alt Main',
+    info: ['alt-spread', 'alt-total'],
     id: 3,
   },
   {
     title: 'Goal Scorer',
+    info: ['goal-scorer'],
     id: 4,
   },
   {
     title: 'Player Props',
+    info: ['player-props'],
     id: 5,
   },
   {
     title: 'Goalie Props',
+    info: ['goalie-props'],
     id: 6,
   },
   {
     title: 'Periods',
+    info: ['period-lines', 'first-x-periods'],
     id: 7,
   },
   {
     title: 'Game Props',
+    info: ['game-props', 'alt-game-props'],
     id: 8,
   },
 ]
