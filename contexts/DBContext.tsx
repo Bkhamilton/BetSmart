ecdefc0a3fe64abfc6c3b1c5bea14ba17dce8bfa
Bkhamilton <bkhamilton310@gmail.com>
2024-06-15 14:54:49 -0400

    creating DBContext Context to hold static DB data

---
+++ contexts/DBContext.tsx
@@ -0,0 +1,64 @@
+// app/contexts/BetContext/BetContext.tsx
+import React, { createContext, ReactNode, useEffect } from 'react';
+import { useSQLiteContext } from 'expo-sqlite';
+import { getAllBookies } from '@/db/general/Bookies';
+import { getAllLeagues } from '@/db/general/Leagues';
+
+interface Bookie {
+    id: number;
+    name: string;
+    description: string;
+}
+
+interface League {
+    id: number;
+    leagueName: string;
+    sport: string;
+    description: string;
+}
+
+interface DBContextValue {
+    db: any;
+    bookies: Bookie[];
+    leagues: League[];
+}
+
+export const DBContext = createContext<DBContextValue>({
+    db: null,
+    bookies: [],
+    leagues: [],
+});
+
+interface DBContextValueProviderProps {
+  children: ReactNode;
+}
+
+export const DBContextProvider = ({ children }: DBContextValueProviderProps) => {
+    const db = useSQLiteContext();
+
+    const [bookies, setBookies] = React.useState<Bookie[]>([]);
+    const [leagues, setLeagues] = React.useState<League[]>([]);
+
+    useEffect(() => {
+        if (db) {
+            getAllBookies(db).then((result) => {
+                setBookies(result);
+            });
+            getAllLeagues(db).then((result) => {
+                setLeagues(result);
+            });
+        }
+    }, [db]);
+
+    const value = {
+        db,
+        bookies,
+        leagues,
+    };
+
+    return (
+        <DBContext.Provider value={value}>
+            {children}
+        </DBContext.Provider>
+    );
+};
