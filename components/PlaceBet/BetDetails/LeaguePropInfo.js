11f3df25e9c031689f7e3f5ee72e8f40cfabcebc
Bkhamilton <bkhamilton310@gmail.com>
2024-06-15 21:23:05 -0400

    started display for leaguePropInfo information

--- components/PlaceBet/BetDetails/LeaguePropInfo.js
+++ components/PlaceBet/BetDetails/LeaguePropInfo.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { TouchableOpacity, Text, View } from '@/components/Themed';
+import { Text, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 
-export default function LeaguePropInfo({ leagueProp }) {
+export default function LeaguePropInfo({ leaguePropInfo }) {
 
     return (
         <View>
-            {/* Main Block */}
-            <View>
-
-            </View>
-            {/* Alt Category Block */}
-            <View>
-
-            </View>
+            {leaguePropInfo.map((info, index) => (
+                <View key={index} style={styles.container}>
+                    <Text style={{ fontSize: 16 }}>{info.propValue}</Text>
+                </View>
+            ))}
         </View>
     );
 }
