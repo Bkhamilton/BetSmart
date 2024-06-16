a97b60a459755925e6d44eecbe8f4209d506a81b
Bkhamilton <bkhamilton310@gmail.com>
2024-06-15 22:43:55 -0400

    working on displayfor BetSlip Bets and Legs

--- components/Modals/BetSlipModal.js
+++ components/Modals/BetSlipModal.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { BetContext } from '@/contexts/BetContext/BetContext';
 import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { View, Text, TouchableOpacity, Modal } from '@/components/Themed';
 import { getDate, getTime, getAmPm } from '@/utils/dateFunctions';
 
-export default function BetSlipModal({ visible, close, betslip }) {
+export default function BetSlipModal({ visible, close }) {
+
+    const { betSlip } = useContext(BetContext);
 
     const Leg = ({ leg }) => {
         return (
             <View>
                 <Text>{leg.type}</Text>
-                <Text>{leg.betTarget}</Text>
-                <Text>{leg.stat}</Text>
-                <Text>{leg.line}</Text>
+                <Text>{leg.betTarget} {leg.stat} {leg.line}</Text>
                 <Text>{leg.odds}</Text>
             </View>
         );
@@ -42,26 +43,20 @@ export default function BetSlipModal({ visible, close, betslip }) {
             <TouchableWithoutFeedback
                 onPress={close}
             >
-                <View style={styles.modalContainer}>
+                <View style={styles.container}>
                     <View style={styles.modalContent}>
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                             <Text style={styles.modalText}>
-                                {getDate(betslip.date) + ' ' + getTime(betslip.date) + ' ' + getAmPm(betslip.date)}
+                                {getDate(betSlip.date) + ' ' + getTime(betSlip.date) + ' ' + getAmPm(betSlip.date)}
                             </Text>
                             <Text style={styles.modalText}>
-                                {betslip.odds}
+                                {betSlip.odds}
                             </Text>
                         </View>
-                        <Text style={styles.modalText}>
-                            {betslip.type}
-                        </Text>
-                        <Text style={styles.modalText}>
-                            {betslip.betAmount}
-                        </Text>
-                        <Text style={styles.modalText}>
-                            {getTime(betslip.date) + ' ' + getAmPm(betslip.date) + ' ' + getDate(betslip.date)}
-                        </Text>
-                        {betslip.bets.map((bet, index) => (
+                        <Text style={styles.modalText}>{betSlip.type}</Text>
+                        <Text style={styles.modalText}>{betSlip.betAmount}</Text>
+                        <Text style={styles.modalText}>{getTime(betSlip.date) + ' ' + getAmPm(betSlip.date) + ' ' + getDate(betSlip.date)}</Text>
+                        {betSlip.bets.map((bet, index) => (
                             <Bet key={index} bet={bet} />
                         ))}
                         <TouchableOpacity style={styles.closeButton} onPress={close}>
@@ -75,7 +70,7 @@ export default function BetSlipModal({ visible, close, betslip }) {
 }
 
 const styles = StyleSheet.create({
-    modalContainer: {
+    container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -83,7 +78,9 @@ const styles = StyleSheet.create({
     },
     modalContent: {
         backgroundColor: 'white',
-        padding: 20,
+        width: '100%',
+        height: '100%',
+        paddingTop: 50,
         borderRadius: 10,
         alignItems: 'center',
     },
