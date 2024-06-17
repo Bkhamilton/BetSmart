a3ec6a3ae4891f681ca62ed1ed848ba2b285db13
Bkhamilton <bkhamilton310@gmail.com>
2024-06-16 21:20:10 -0400

    add componentMapping to display components based on leaguePropInfo

--- components/PlaceBet/BetDetails/LeaguePropInfo.js
+++ components/PlaceBet/BetDetails/LeaguePropInfo.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import AltLine from './LeaguePropTypes/AltLine';
+import MainLine from './LeaguePropTypes/MainLine';
 import Colors from '@/constants/Colors';
 
 export default function LeaguePropInfo({ leaguePropInfo }) {
 
+    const componentMapping = {
+        'alt-line': <AltLine />,
+        'main-line': <MainLine />,
+        'game-lines': <Text>Game Lines</Text>,
+        'first-basket': <Text>First Basket</Text>,
+        'player-special': <Text>Player Special</Text>,
+        'team-special': <Text>Team Special</Text>,
+        'alt-spread': <Text>Alternate Spread</Text>,
+        'alt-total': <Text>Alternate Total</Text>,
+        'alt-player-line': <Text>Alternate Player Line</Text>,
+        'first-to-reach': <Text>First to Reach</Text>,
+        'stat-leaders': <Text>Stat Leaders</Text>,
+        'half-lines': <Text>Half Lines</Text>,
+        'quarter-lines': <Text>Quarter Lines</Text>,
+        'team-props': <Text>Team Props</Text>,
+        'alt-game-props': <Text>Alternate Game Props</Text>,
+        'first-to-x': <Text>First to X</Text>,
+        'game-props': <Text>Game Props</Text>,
+        'first-to-score': <Text>First to Score</Text>,
+        'hit-props': <Text>Hit Props</Text>,
+        'hr-props': <Text>HR Props</Text>,
+        'rbi-props': <Text>RBI Props</Text>,
+        'run-props': <Text>Run Props</Text>,
+        'total-bases': <Text>Total Bases</Text>,
+        'pitcher-props': <Text>Pitcher Props</Text>,
+        'strikeout-props': <Text>Strikeout Props</Text>,
+        'first-x-innings': <Text>First X Innings</Text>,
+        'innings-lines': <Text>Innings Lines</Text>,
+        'first-goal': <Text>First Goal</Text>,
+        'goal-scorer': <Text>Goal Scorer</Text>,
+        'player-props': <Text>Player Props</Text>,
+        'goalie-props': <Text>Goalie Props</Text>,
+        'first-x-periods': <Text>First X Periods</Text>,
+        'period-lines': <Text>Period Lines</Text>,
+    }
+
     return (
-        <View>
-            {leaguePropInfo.map((info, index) => (
-                <View key={index} style={styles.container}>
-                    <Text style={{ fontSize: 16 }}>{info.propValue}</Text>
-                </View>
-            ))}
-        </View>
+        <>
+            {leaguePropInfo.map((info, index) => {
+    
+                return (
+                    <View key={index} style={{ borderWidth: 1, width: '100%' }}>
+                        {componentMapping[info.propValue]}
+                    </View>
+                );
+            })}
+        </>
     );
 }
 
---
+++ components/PlaceBet/BetDetails/LeaguePropTypes/AltLine.js
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, View } from '@/components/Themed';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { BetContext } from '@/contexts/BetContext/BetContext';
+import useTheme from '@/hooks/useTheme';
+
+export default function AltLine() {
+
+    const { league, currentGame } = useContext(BetContext);
+
+    const { iconColor } = useTheme();
+
+    return (
+        <View style={styles.container}>
+            <View style={{ justifyContent: 'center', paddingHorizontal: 8, }}>
+                <FontAwesome5 name="chevron-down" size={16} color={iconColor} />
+            </View>
+            <View>
+                <Text style={{ fontSize: 16 }}>Alternate Line</Text>
+            </View>
+            <View>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: '100%',
+    },
+});
