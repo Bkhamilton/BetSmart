812016a91f057fd7760bbd869c1b8b5ff8c05eac
Bkhamilton <bkhamilton310@gmail.com>
2024-06-22 20:52:39 -0400

    chore: Refactor PropBanner component for improved readability and added prop details view

--- components/PlaceBet/BetDetails/LeaguePropTypes/PropBanner/PropBanner.js
+++ components/PlaceBet/BetDetails/LeaguePropTypes/PropBanner/PropBanner.js
@@ -4,7 +4,7 @@ import { Text, View, TouchableOpacity } from '@/components/Themed';
 import { FontAwesome5 } from '@expo/vector-icons';
 import useTheme from '@/hooks/useTheme';
 
-export default function PropBanner({ title }) {
+export default function PropBanner({ title, type }) {
     const { iconColor } = useTheme();
     const [detailsOpen, setDetailsOpen] = useState(false);
 
@@ -12,21 +12,56 @@ export default function PropBanner({ title }) {
         setDetailsOpen(!detailsOpen);
     };
 
+    const PlayerDisplay = () => {
+        return (
+            <View style={{ width: '100%', paddingHorizontal: 8, paddingVertical: 4 }}>
+                <Text style={{ fontSize: 16 }}>{title}</Text>
+            </View>
+        )
+    }
+
+    const AltDisplay = () => {
+        return (
+            <View style={{ width: '100%', paddingHorizontal: 8, paddingVertical: 4 }}>
+                <Text style={{ fontSize: 16 }}>{title}</Text>
+            </View>
+        )
+    }
+
+    const MainDisplay = () => {
+        return (
+            <View style={{ width: '100%', paddingHorizontal: 8, paddingVertical: 4 }}>
+                <Text style={{ fontSize: 16 }}>{title}</Text>
+            </View>
+        )
+    }
+
+    // Mapping of type to display components
+    const displayMapping = {
+        Player: PlayerDisplay,
+        Alt: AltDisplay,
+        Alternate: AltDisplay,
+        Main: MainDisplay,
+    };
+
+    // Function to get the display component based on type
+    const getDisplayComponent = () => {
+        // Use MainDisplay as a fallback if type is not found in the mapping
+        const DisplayComponent = displayMapping[type] || MainDisplay;
+        return <DisplayComponent />;
+    };
+
     return (
         <View style={{ paddingVertical: 6, width: '100%' }}>
             <TouchableOpacity onPress={handlePress} style={styles.propContainer}>
                 <View style={{ justifyContent: 'center', paddingHorizontal: 8, }}>
-                    <FontAwesome5 name="chevron-down" size={16} color={iconColor} />
+                    <FontAwesome5 name={detailsOpen ? "chevron-up" : "chevron-down"} size={16} color={iconColor} />
                 </View>
                 <View>
                     <Text style={{ fontSize: 16 }}>{title}</Text>
                 </View>
-                {detailsOpen && (
-                    <View>
-                        {/* Detailed view goes here */}
-                    </View>
-                )}
             </TouchableOpacity>
+            {detailsOpen && getDisplayComponent()}
         </View>
     );
 }
