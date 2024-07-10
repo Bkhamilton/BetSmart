d45d48fbd8d2edabacdeea942f85367ab867702c
Bkhamilton <bkhamilton310@gmail.com>
2024-07-09 23:18:47 -0400

    added streak prop to track Hot, Cold, and No streaks

--- app/(tabs)/insights.js
+++ app/(tabs)/insights.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { Text, View, TouchableOpacity } from '@/components/Themed';
 import Header from '@/components/Header/Header';
 import { useSQLiteContext } from 'expo-sqlite';
 import { retrieveMarketsDB } from '@/api/prop-odds/markets';
@@ -8,8 +8,13 @@ import { getLeagueByName } from '@/db/general/Leagues';
 import InsightIntro from '@/components/Insights/InsightIntro/InsightIntro';
 import LossAnalysis from '@/components/Insights/LossAnalysis/LossAnalysis';
 import { getTodaysGameswithNames } from '@/db/general/Games';
+import WinAnalysis from '@/components/Insights/WinAnalysis/WinAnalysis';
+import { FontAwesome } from '@expo/vector-icons';
+import useTheme from '@/hooks/useTheme';
 
 export default function InsightScreen() {
+
+  const [streak, setStreak] = useState('hot');
 
   const saveForLater = () => {
     const date = '2024-05-27'
@@ -23,15 +28,35 @@ export default function InsightScreen() {
       });
     });
   }
+  
+  // function to cycle betwen hot, cold, and no streaks
+  const cycleStreak = () => {
+    if (streak === 'hot') {
+      setStreak('cold');
+    } else if (streak === 'cold') {
+      setStreak('');
+    } else {
+      setStreak('hot');
+    }
+  }
+
+  const { iconColor } = useTheme();
 
   const db = useSQLiteContext();
 
   return (
     <>
-      <Header title={"Insights"} />
+      <Header title={"Insights"}>
+        <TouchableOpacity
+          onPress={cycleStreak}
+          style={{ marginRight: 10 }}
+        >
+          <FontAwesome name="refresh" size={20} color={iconColor} />
+        </TouchableOpacity>
+      </Header>
       <View>
-        <InsightIntro />
-        <LossAnalysis />
+        {streak && <InsightIntro streak={streak}/>}
+        {streak === 'hot' ? <WinAnalysis /> : <LossAnalysis />}
       </View>
     </>
   );
