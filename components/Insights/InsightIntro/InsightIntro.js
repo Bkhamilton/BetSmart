3a91f6419970070d8b73e77e652b45a3548f9ee6
Bkhamilton <bkhamilton310@gmail.com>
2024-07-09 22:23:09 -0400

    chore: Refactor InsightIntro component to use theme colors and improve styling

--- components/Insights/InsightIntro/InsightIntro.js
+++ components/Insights/InsightIntro/InsightIntro.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { TouchableOpacity, Text, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
+import { FontAwesome5 } from '@expo/vector-icons';
+import useTheme from '@/hooks/useTheme';
 
 export default function InsightIntro({ props }) {
+
+  const { redText } = useTheme();
+
   return (
     <View style={styles.container}>
-      <View style={{ borderWidth: 1 }}>
-        <Text style={{ fontSize: 16 }}>Hot Streak</Text>
+      <View style={[styles.streakContainer, { borderColor: redText }]}>
+        <FontAwesome5 name="fire" size={20} color={redText} style={{ marginHorizontal: 2 }}/>
+        <Text style={{ fontSize: 20, fontWeight: '500', color: redText }}>Hot Streak</Text>
       </View>
     </View>
   );
@@ -15,8 +21,17 @@ export default function InsightIntro({ props }) {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    paddingVertical: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 8,
   },
+  streakContainer: {
+    borderWidth: 2,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
