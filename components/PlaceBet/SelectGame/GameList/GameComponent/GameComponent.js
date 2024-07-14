0df291eff0d437d424c456873b21c77a256f0c30
Ben Hamilton <43786988+Bkhamilton@users.noreply.github.com>
2024-07-14 09:47:29 -0400

    Update GameComponent.js

--- components/PlaceBet/SelectGame/GameList/GameComponent/GameComponent.js
+++ components/PlaceBet/SelectGame/GameList/GameComponent/GameComponent.js
@@ -37,7 +37,7 @@ export default function GameComponent({ game, selectGame, selectProp }) {
         fetchLogos();
     }, [game]);
     
-        useEffect(() => {
+    useEffect(() => {
         fetchMarketProps();
     }, []);
 
