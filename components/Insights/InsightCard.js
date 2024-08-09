9baf61023fdebd307df6f7c2d72183be159f563c
Bkhamilton <bkhamilton310@gmail.com>
2024-08-09 12:46:28 -0400

    started edits on InsightCard design

--- components/Insights/InsightCard.js
+++ components/Insights/InsightCard.js
@@ -4,10 +4,15 @@ import { TouchableOpacity, Text, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 
 export default function InsightCard({ title }) {
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={{ fontSize: 16 }}>{title}</Text>
+        <Text style={{ fontSize: 14, fontWeight: '500' }}>This is where a description of the title will go.</Text>
+      </View>
+      <View>
+        {/* Example Bets Here */}
       </View>
     </View>
   );
@@ -17,6 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderWidth: 1,
-    paddingHorizontal: 12,
+    paddingHorizontal: 16,
   },
 });
