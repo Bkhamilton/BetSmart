c8592bfb1a78dd01697724d49dd5318fd22eeee9
Bkhamilton <bkhamilton310@gmail.com>
2024-09-18 16:21:08 -0400

    created bookieConstants to hold bookieImages

---
+++ constants/bookieConstants.js
@@ -0,0 +1,9 @@
+import draftkings from '@/assets/images/DraftKings.png';
+import fanduel from '@/assets/images/FanDuel.jpg';
+import betMGM from '@/assets/images/BetMGM.jpg';
+
+export const bookieImages = {
+    'DraftKings': draftkings,
+    'FanDuel': fanduel,
+    'BetMGM': betMGM,
+};
