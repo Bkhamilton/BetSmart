0ff8d3202a4f9bb8f0951d4b5ac3b0915d42fe31
Bkhamilton <bkhamilton310@gmail.com>
2024-10-02 13:18:59 -0400

    working on getting getFavorite function for each Type, currently at 2/5

--- components/Profile/UserFavorites.js
+++ components/Profile/UserFavorites.js
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { TouchableOpacity, Text, View, ScrollView } from '@/components/Themed';
 import useTheme from '@/hooks/useTheme';
+import { UserContext } from '@/contexts/UserContext';
+import { useSQLiteContext } from 'expo-sqlite';
+import { getFavoriteBookie } from '@/db/betslips/BetSlips';
+import { getFavoriteLeague } from '@/db/betslips/ParticipantBets';
 
 export default function UserFavorites({ league, team, player, sportsbook, bet }) {
 
+    const { user } = useContext(UserContext);
+
+    const db = useSQLiteContext();
+
     const { grayBackground, grayBorder } = useTheme();
 
+    const getFavorite = async (type) => {
+        switch (type) {
+            case 'League':
+                return getFavoriteLeague(db, user.id);
+            case 'Team':
+                return team;
+            case 'Player':
+                return player;
+            case 'Bookie':
+                return getFavoriteBookie(db, user.id);
+            case 'Bet':
+                return bet;
+            default:
+                return null;
+        }
+    }
+
     const FavoriteComponent = ({ type }) => {
+
+        const [favorite, setFavorite] = useState(null);
+
+        useEffect(() => {
+            getFavorite(type).then((res) => setFavorite(res));
+        }, []);
+
         return (
             <View style={[styles.favoriteContainer, { backgroundColor: grayBackground, borderColor: grayBorder }]}>
                 <View style={{ alignItems: 'center', backgroundColor: 'transparent', paddingTop: 4 }}>
                     <Text style={styles.favoriteText}>Top {type}</Text>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{JSON.stringify(favorite)}</Text>
                 </View>
             </View>
         );
     };
 
     return (
-        <View style={styles.container}>
+        <>
             <View style={{ paddingHorizontal: 20 }}>
                 <Text style={{ fontSize: 18, fontWeight: '500' }}>Favorites</Text>
             </View>
             <ScrollView 
                 horizontal 
                 showsHorizontalScrollIndicator={false}
-                style={{ paddingVertical: 12, paddingLeft: 20 }}
+                style={{ paddingVertical: 12, paddingLeft: 12 }}
             >
                 <FavoriteComponent type="League" />
                 <FavoriteComponent type="Team" />
@@ -33,15 +66,12 @@ export default function UserFavorites({ league, team, player, sportsbook, bet })
                 <FavoriteComponent type="Bookie" />
                 <FavoriteComponent type="Bet" />
             </ScrollView>
-        </View>
+        </>
     );
   
 }
 
   const styles = StyleSheet.create({
-    container: {
-      paddingHorizontal: 0,
-    },
     favoriteContainer: {
         borderWidth: 1,
         borderRadius: 8,
