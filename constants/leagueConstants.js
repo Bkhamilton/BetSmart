4132118ff1b78b1455767cdd94be052ec4db7d28
Bkhamilton <bkhamilton310@gmail.com>
2024-10-03 12:27:39 -0400

    added leagueConstants to store mapping to display league images

---
+++ constants/leagueConstants.js
@@ -0,0 +1,19 @@
+import mlb from '@/assets/images/mlb.png';
+import nba from '@/assets/images/nba.png';
+import nhl from '@/assets/images/nhl.png';
+import nfl from '@/assets/images/nfl.png';
+import mls from '@/assets/images/mls.png';
+import ncaa from '@/assets/images/ncaa.png';
+import pga from '@/assets/images/pga.png';
+import ufc from '@/assets/images/ufc.png';
+
+export const leagueImages = {
+    'NBA': nba,
+    'NHL': nhl,
+    'MLB': mlb,
+    'NFL': nfl,
+    'MLS': mls,
+    'NCAAB': ncaa,
+    'PGA': pga,
+    'UFC': ufc,
+};
