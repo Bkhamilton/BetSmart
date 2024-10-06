95cf67390e59f459749960c8015e6800234906c2
Bkhamilton <bkhamilton310@gmail.com>
2024-10-06 19:56:23 -0400

    Created AddBookie component to add new bookies to userBalance

---
+++ components/Modals/AddBookie.js
@@ -0,0 +1,77 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { StyleSheet, Modal, Image } from 'react-native';
+import { TouchableOpacity, Text, View } from '@/components/Themed';
+import { DBContext } from '@/contexts/DBContext';
+import { UserContext } from '@/contexts/UserContext';
+import { bookieImages } from '@/constants/bookieConstants';
+import useTheme from '@/hooks/useTheme';
+
+export default function AddBookie({ visible, close, selectBookie }) {
+
+    const { grayBackground, grayBorder } = useTheme();
+
+    const { bookies } = useContext(DBContext);
+
+    const { userBalance } = useContext(UserContext);
+
+    const [validBookies, setValidBookies] = useState([]);
+
+    useEffect(() => {
+        const validBookies = bookies.filter(bookie => !userBalance.find(b => b.bookieName === bookie.name));
+        setValidBookies(validBookies);
+    }, [bookies, userBalance]);
+
+    const BookieButton = ({ bookie }) => {
+        return (
+            <TouchableOpacity
+                style={[styles.bookieButton, { backgroundColor: grayBackground, borderColor: grayBorder }]}
+                onPress={() => selectBookie(bookie)}
+            >
+                <Image source={bookieImages[bookie]} style={{ width: 40, height: 40, borderRadius: 8 }} />
+                <Text>{bookie}</Text>
+            </TouchableOpacity>
+        );
+    }
+
+    return (
+        <Modal
+            animationType="fade"
+            transparent={true}
+            visible={visible}
+            onRequestClose={close}
+        >
+            <View style={styles.container}>
+                <View style={{ backgroundColor: 'transparent' }}>
+                    <TouchableOpacity 
+                        onPress={close}
+                        style={{ padding: 8 }}
+                    >
+                        <Text>Close</Text>
+                    </TouchableOpacity>
+                    {validBookies.map(bookie => <BookieButton key={bookie.id} bookie={bookie.name} />)}
+                </View>
+            </View>
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    leagueContainer: {
+        padding: 10,
+        borderWidth: 1,
+    },
+    bookieButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 10,
+    },
+});
