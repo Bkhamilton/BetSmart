8a0142eac3d5133b7b662dbccb87d6d1b42969b5
Bkhamilton <bkhamilton310@gmail.com>
2024-10-06 21:29:14 -0400

    cleaned up and refactored component, updating styling

--- components/Insights/InsightCard.js
+++ components/Insights/InsightCard.js
@@ -5,29 +5,39 @@ import useTheme from '@/hooks/useTheme';
 
 export default function InsightCard({ title }) {
 
-  const { name, description } = title;
+    const { name, description } = title;
 
-  const { grayBackground, grayBorder } = useTheme();
+    const { grayBackground, grayBorder } = useTheme();
 
-  return (
-    <View style={[styles.container, { backgroundColor: grayBackground, borderColor: grayBorder }]}>
-      <ClearView>
-        <Text style={{ fontSize: 16 }}>{name}</Text>
-        <Text style={{ fontSize: 14, fontWeight: '500', width: 300 }}>{description}</Text>
-      </ClearView>
-      <ClearView>
-        {/* Example Bets Here */}
-      </ClearView>
-    </View>
-  );
+    return (
+        <View style={[styles.container, { backgroundColor: grayBackground, borderColor: grayBorder }]}>
+            <ClearView>
+                <Text style={styles.nameText}>{name}</Text>
+                <Text style={styles.descriptionText}>{description}</Text>
+            </ClearView>
+            <ClearView>
+                {/* Example Bets Here */}
+            </ClearView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderWidth: 1,
-    paddingHorizontal: 16,
-    height: 120,
-    borderRadius: 8,
-  },
+    container: {
+        flex: 1,
+        borderWidth: 1,
+        paddingHorizontal: 12,
+        height: 120,
+        borderRadius: 8,
+        paddingTop: 8,
+    },
+    nameText: {
+        fontSize: 16,
+        fontWeight: '500',
+    },
+    descriptionText: {
+        fontSize: 14,
+        fontWeight: '400',
+        width: 300,
+    },
 });
