4bf3df5dff1b727da1c8332e11f2824f9e6093e9
Bkhamilton <bkhamilton310@gmail.com>
2024-10-07 14:52:54 -0400

    added Barstool, Caesars, and PointsBet as valid bookies to DB

--- constants/bookieConstants.js
+++ constants/bookieConstants.js
@@ -1,9 +1,15 @@
 import draftkings from '@/assets/images/DraftKings.png';
 import fanduel from '@/assets/images/FanDuel.jpg';
 import betMGM from '@/assets/images/BetMGM.jpg';
+import caesars from '@/assets/images/Caesars.png';
+import pointsbet from '@/assets/images/PointsBet.png';
+import barstool from '@/assets/images/Barstool.png';
 
 export const bookieImages = {
     'DraftKings': draftkings,
     'FanDuel': fanduel,
     'BetMGM': betMGM,
+    'Caesars': caesars,
+    'PointsBet': pointsbet,
+    'Barstool': barstool,
 };
