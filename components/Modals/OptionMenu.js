1ebad3c948b066376d25fd83b6312a836722b9a2
Bkhamilton <bkhamilton310@gmail.com>
2024-10-11 20:57:53 -0400

    created OptionMenu modal to display options

---
+++ components/Modals/OptionMenu.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
+import { TouchableOpacity, Text, View, Modal } from '@/components/Themed';
+import useTheme from '@/hooks/useTheme';
+
+export default function OptionMenu({ visible, close, options, selectOption }) {
+    return (
+        <Modal
+            animationType="fade"
+            transparent={true}
+            visible={visible}
+            onRequestClose={close}
+        >
+            <TouchableWithoutFeedback onPress={close}>
+                <View style={styles.container}>
+                    <View style={styles.optionBox}>
+                        {options.map((option, index) => (
+                            <TouchableOpacity key={index} style={styles.option} onPress={() => selectOption(option)}>
+                                <Text style={styles.optionText}>{option}</Text>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    optionBox: {
+        padding: 16,
+        borderRadius: 8,
+    },
+    option: {
+        padding: 8,
+        borderRadius: 8,
+        borderWidth: 1,
+        marginBottom: 8,
+    },
+    optionText: {
+        fontSize: 16,
+    },
+});
