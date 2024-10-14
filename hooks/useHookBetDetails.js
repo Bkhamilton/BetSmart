ac7278e1be9ce08433f012fbb84879b22e855a87
Bkhamilton <bkhamilton310@gmail.com>
2024-10-14 11:17:39 -0400

    created hook useHookBetDetails to track states in betDetails page

---
+++ hooks/useHookBetDetails.js
@@ -0,0 +1,46 @@
+import { useState, useEffect, useContext } from 'react';
+import { useSQLiteContext } from 'expo-sqlite';
+import { getLeaguePropsForLeague } from '@/db/bet-general/LeagueProps';
+import { getLeaguePropInfo } from '@/db/bet-general/LeaguePropsInfo';
+import { BetContext } from '@/contexts/BetContext/BetContext';
+
+const useHookBetDetails = () => {
+
+    const db = useSQLiteContext();
+
+    const { league } = useContext(BetContext);
+
+    const [leagueProps, setLeagueProps] = useState([]); // [ { id: 0, leagueId: 0, propName: '' }
+    const [leaguePropInfo, setLeaguePropInfo] = useState([]); // [ { id: 0, leagueId: 0, propName: '', propValue: '' }
+  
+    const [curLeagueProp, setCurLeagueProp] = useState('Popular');
+  
+    const selectLeagueProp = (prop) => {
+      setCurLeagueProp(prop);
+    };
+  
+    useEffect(() => {
+      getLeaguePropsForLeague(db, league.id).then((props) => {
+        const sortedProps = props.sort((a, b) => a.id - b.id);
+        setLeagueProps(sortedProps);
+      });
+    }, []);
+  
+    useEffect(() => {
+      if (curLeagueProp) {
+        getLeaguePropInfo(db, league.id, curLeagueProp).then((info) => {
+          const sortedInfo = info.sort((a, b) => a.id - b.id);
+          setLeaguePropInfo(sortedInfo);
+        });
+      }
+    }, [curLeagueProp]);
+
+    return {
+        leagueProps,
+        leaguePropInfo,
+        curLeagueProp,
+        selectLeagueProp,
+    };
+};
+
+export default useHookBetDetails;
