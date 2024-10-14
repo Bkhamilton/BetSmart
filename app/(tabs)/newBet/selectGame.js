ec381083652dc3405fb4e5aebf965156e7900a76
Bkhamilton <bkhamilton310@gmail.com>
2024-10-14 11:19:11 -0400

    moved state management logic to useHookSelectGame

--- app/(tabs)/newBet/selectGame.js
+++ app/(tabs)/newBet/selectGame.js
@@ -1,11 +1,7 @@
-import { useEffect, useState, useContext } from 'react';
+import { useContext } from 'react';
 import { StyleSheet } from 'react-native';
-import { retrieveGamesDate, retrieveAllGames } from '@/api/prop-odds/games.js';
 import { BetContext } from '@/contexts/BetContext/BetContext';
 import { DBContext } from '@/contexts/DBContext'; 
-import { UserContext } from '@/contexts/UserContext';
-import { removeLeg, updateBetSlipAmounts, updateBetSlipBookie } from '@/contexts/BetContext/betSlipHelpers';
-import { useSQLiteContext } from 'expo-sqlite';
 import { Text, View } from '@/components/Themed';
 import MainButtons from '@/components/PlaceBet/SelectGame/MainButtons';
 import GameList from '@/components/PlaceBet/SelectGame/GameList/GameList';
@@ -18,15 +14,13 @@ import SelectLeague from '@/components/Modals/SelectLeague';
 import useTheme from '@/hooks/useTheme';
 import useRouting from '@/hooks/useRouting';
 import useHookNewBet from '@/hooks/useHookNewBet';
+import useHookSelectGame from '@/hooks/useHookSelectGame';
 import DatePicker from '@/components/PlaceBet/SelectGame/DatePicker';
 
 export default function SelectGameScreen() {
 
-  const db = useSQLiteContext();
-
-  const { league, setLeague, betSlip, confirmBetSlip } = useContext(BetContext);
+  const { league, betSlip } = useContext(BetContext);
   const { leagues } = useContext(DBContext);
-  const { setTrigger } = useContext(UserContext);
 
   const { handleSelectGame } = useRouting();
 
@@ -43,85 +37,22 @@ export default function SelectGameScreen() {
     confirmBet,
   } = useHookNewBet();
 
-  const today = new Date();
-  const formattedToday = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-  const [date, setDate] = useState(formattedToday);
-
-  const [header, setHeader] = useState('Place Bet');
-
-  const [allSportsData, setAllSportsData] = useState([]);
-  const [leagueSelected, setLeagueSelected] = useState(false);
-
-  const [totalLegs, setTotalLegs] = useState(0);
-
-  const [selectLeagueModal, setSelectLeagueModal] = useState(false);
-
-  const [refreshing, setRefreshing] = useState(false);
-
-  const onRefresh = () => {
-    setRefreshing(true);
-
-    setTrigger(prev => !prev);
-
-    setRefreshing(false);
-  }
-
-  const openSelectLeagueModal = () => {
-    setSelectLeagueModal(true);
-  }
-
-  const closeSelectLeagueModal = () => {
-    setSelectLeagueModal(false);
-  }
-
-  const selectLeague = (newLeague) => {
-    if (league?.leagueName === newLeague.leagueName) {
-      setLeague({});
-      setHeader('Place Bet');
-      setLeagueSelected(false);
-    } else {
-      setLeague(newLeague);
-      setHeader(newLeague.leagueName);
-      setLeagueSelected(true);
-    }
-  }
-
-  const onConfirmBet = (wager, winnings, bookieId) => {
-    confirmBet(wager, winnings, bookieId);
-    onRefresh();
-  }
-
-  const updateDate = (direction) => {
-    const currentDate = new Date(date); // Get the current date
-
-    // Check the direction parameter
-    if (direction === 'prev') {
-      currentDate.setDate(currentDate.getDate()); // Set the date to the previous day
-    } else if (direction === 'next') {
-      currentDate.setDate(currentDate.getDate() + 2); // Set the date to the next day
-    } else {
-      console.log('Invalid direction parameter. Please use "prev" or "next".');
-      return; // Exit the function if the direction parameter is invalid
-    }
-
-    // Update the date state with the new date
-    setDate(`${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')}`);
-  }
-
-  useEffect(() => {
-    const fetchSportsData = async () => {
-      retrieveAllGames(db, date).then((games) => {
-        setAllSportsData(games);
-      });
-    };
-  
-    fetchSportsData();
-  }, [date]);
+  const {
+    date,
+    header,
+    selectLeagueModal,
+    leagueSelected,
+    allSportsData,
+    openSelectLeagueModal,
+    closeSelectLeagueModal,
+    updateDate,
+    selectLeague,
+  } = useHookSelectGame();
 
   const curLeagueData = allSportsData?.find(sportData => sportData.sport === league?.leagueName);
   const curLeagueGames = curLeagueData ? curLeagueData.data : [];
 
-  const { grayBorder, iconColor } = useTheme();
+  const { grayBorder } = useTheme();
 
   const SelectGameHeader = () => {
     return (
@@ -154,7 +85,7 @@ export default function SelectGameScreen() {
             close={closeBetSlipModal}
             removeProp={removeProp}
             removeBetSlip={removeBetSlip}
-            confirm={onConfirmBet}
+            confirm={confirmBet}
           />
         )
       }
