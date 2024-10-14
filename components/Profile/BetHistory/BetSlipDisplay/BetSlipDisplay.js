96ed5ad5d5ce52a0a401a2d67c59028d7e5415a6
Bkhamilton <bkhamilton310@gmail.com>
2024-10-14 17:20:28 -0400

    started work of implementing BetSlipDisplay

--- components/Profile/BetHistory/BetSlipDisplay/BetSlipDisplay.js
+++ components/Profile/BetHistory/BetSlipDisplay/BetSlipDisplay.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { TouchableOpacity, Text, View } from '@/components/Themed';
-import Colors from '@/constants/Colors';
+import { Text, View } from '@/components/Themed';
 
 export default function BetSlipDisplay({ betSlips, selectedType }) {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={{ fontSize: 16 }}>ComponentContent</Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={{ fontSize: 16 }}>{selectedType} Bets</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    paddingVertical: 1,
-  },
+    container: {
+        paddingHorizontal: 12,
+        paddingVertical: 4,
+    },
 });
