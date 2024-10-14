db817ac0b0ad2efdf53e7456270a98c76a3789c6
Bkhamilton <bkhamilton310@gmail.com>
2024-10-14 16:17:34 -0400

    using memoization, updated batch sizes, and efficient key management, optimized performance for the GameList GameComponents

--- components/PlaceBet/SelectGame/GameList/GameList.js
+++ components/PlaceBet/SelectGame/GameList/GameList.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { View, Text } from '@/components/Themed'
 import GameComponent from '@/components/PlaceBet/SelectGame/GameList/GameComponent/GameComponent';
 
 export default function GameList({ games, selectGame }) {
+    const renderItem = useCallback(({ item }) => (
+        <GameComponent 
+        game={item} 
+        selectGame={selectGame}
+        />
+    ), [selectGame]);
+
+    const keyExtractor = useCallback((item) => item.id.toString(), []);
+
+    const ItemSeparatorComponent = useCallback(() => (
+        <View style={styles.separator} />
+    ), []);
+
     return (
         <View style={styles.container}>
-          {  
+        {  
             games && games.length > 0 ? 
-              <FlatList
+            <FlatList
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ marginBottom: 100 }}
                 data={games}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => (
-                  <GameComponent 
-                    game={item} 
-                    selectGame={selectGame}
-                  />
+                keyExtractor={keyExtractor}
+                renderItem={renderItem}
+                ItemSeparatorComponent={ItemSeparatorComponent}
+                initialNumToRender={8}
+                maxToRenderPerBatch={8}
+                windowSize={21}
+                getItemLayout={(data, index) => (
+                  { length: 140, offset: 140 * index, index }
                 )}
-                ItemSeparatorComponent={() => <View style={styles.separator} />}
-              />
+            />
             : 
-              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={styles.noGameContainer}>
                 <Text>No games available</Text>
-              </View>
-          }
+            </View>
+        }
         </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -39,5 +53,10 @@ const styles = StyleSheet.create({
   separator: {
     height: 4,
     backgroundColor: 'transparent',
+  },
+  noGameContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
