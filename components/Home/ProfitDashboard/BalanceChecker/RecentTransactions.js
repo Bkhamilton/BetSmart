93275e6d148599ac3170a328af17fa11feab869f
Bkhamilton <bkhamilton310@gmail.com>
2024-10-17 21:34:30 -0400

    added box around transactions to eventually allow for viewing of all transactions

--- components/Home/ProfitDashboard/BalanceChecker/RecentTransactions.js
+++ components/Home/ProfitDashboard/BalanceChecker/RecentTransactions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { Text, View, Pressable } from '@/components/Themed';
 
 export default function RecentTransactions({ transactions, bookieId }) {
 
@@ -10,10 +10,22 @@ export default function RecentTransactions({ transactions, bookieId }) {
         return `${transaction.transactionType === 'Deposit' ? '+' : '-'}$${transaction.amount}`;
     }
 
+    const [opacity, setOpacity] = React.useState(1);
+
+    const onPressIn = () => {
+        setOpacity(0.6);
+    }
+
+    const onPressOut = () => {
+        setOpacity(1);
+    }
+
     return (
         <View style={styles.box}>
-            <View 
-                style={styles.transactionContainer}
+            <Pressable 
+                style={[styles.transactionContainer, { opacity}]}
+                onPressIn={onPressIn}
+                onPressOut={onPressOut}
             >
                 {[...bookieTransactions].reverse().slice(0, 6).map((transaction, index) => (
                     <View key={index} style={{ backgroundColor: 'transparent' }}>
@@ -21,7 +33,7 @@ export default function RecentTransactions({ transactions, bookieId }) {
                             fontWeight: transaction.transactionType === 'Deposit' ? '500' : '600' }}>{formatTransaction(transaction)}</Text>
                     </View>
                 ))}
-            </View>
+            </Pressable>
         </View>
     );
 }
@@ -30,13 +42,17 @@ const styles = StyleSheet.create({
     box: {
         backgroundColor: 'transparent',
         flex: 0.26,
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end',
     },
     transactionContainer: {
-        backgroundColor: 'transparent', 
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         alignItems: 'flex-end', 
         justifyContent: 'flex-end',
-        paddingRight: 8,
+        marginRight: 4,
+        paddingHorizontal: 4,
         paddingBottom: 8,
         height: 120,
+        borderRadius: 8,
     },
 });
