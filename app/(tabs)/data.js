6a17a86f91e6a3c41c22537b0c96037039972e9d
Bkhamilton <bkhamilton310@gmail.com>
2024-10-21 14:23:14 -0400

    cleaned up and removed unneeded imports, improved readability and look of page file

--- app/(tabs)/data.js
+++ app/(tabs)/data.js
@@ -1,49 +1,22 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import { Text, View, TouchableOpacity, ScrollView } from '@/components/Themed';
-import Header from '../../components/Header/Header';
-import React, { useState } from 'react';
-import DataHeader from '../../components/Data/DataHeader';
-import TimeSelector from '../../components/Data/TimeSelector';
-import AnalyticsDisplay from '../../components/Data/AnalyticsDisplay';
+import React from 'react';
+import { ScrollView } from '@/components/Themed';
+import DataHeader from '@/components/Data/DataHeader';
+import AnalyticsDisplay from '@/components/Data/AnalyticsDisplay';
+import useHookData from '@/hooks/useHookData';
 
 export default function DataScreen() {
-  const [selectedTime, setSelectedTime] = useState('Open');
-  const [filteredData, setFilteredData] = useState([]);
 
-  const selectTime = (timeRange) => {
-    setSelectedTime(timeRange);
-  };
-
-  const MainHeader = () => {
-    return (
-      <View>
-        <View style={styles.headerContainer}>
-          <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Analytics</Text>
-        </View>
-        <TimeSelector selectTime={selectTime} curTime={selectedTime}/>
-      </View>
-    )
-  };
+  const { selectTime, selectedTime } = useHookData();
 
   return (
-    <View style={styles.container}>
-      <MainHeader/>
+    <>
+      <DataHeader
+        selectTime={selectTime}
+        selectedTime={selectedTime}
+      />
       <ScrollView>
         <AnalyticsDisplay data={selectedTime}/>
       </ScrollView>
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  headerContainer: {
-    height: 84, 
-    paddingHorizontal: 20, 
-    paddingTop: 48,
-    borderBottomWidth: 1,
-    justifyContent: 'center',
-  },
-});
