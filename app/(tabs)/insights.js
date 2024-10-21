a2cabaf9676bf2c75b9a4679b3741a328f4abdce
Bkhamilton <bkhamilton310@gmail.com>
2024-10-21 14:36:44 -0400

    shuffled around import statements for readability purposes

--- app/(tabs)/insights.js
+++ app/(tabs)/insights.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { ScrollView } from '@/components/Themed';
 import { useSQLiteContext } from 'expo-sqlite';
+import { ScrollView } from '@/components/Themed';
+import InsightHeader from '@/components/Insights/InsightHeader';
 import InsightIntro from '@/components/Insights/InsightIntro/InsightIntro';
 import BetAnalysis from '@/components/Insights/BetAnalysis/BetAnalysis';
 import TopBet from '@/components/Insights/TopBet';
-import InsightHeader from '@/components/Insights/InsightHeader';
-
 import useHookInsightsPage from '@/hooks/useHookInsights';
 
 export default function InsightScreen() {
