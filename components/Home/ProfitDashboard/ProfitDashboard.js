74b4a564b897b4b24d1a55cc3975ad6b18f41dc8
Bkhamilton <bkhamilton310@gmail.com>
2024-10-21 20:36:23 -0400

    moved BetSlipResults to its own file and removed unneeded imports

--- components/Home/ProfitDashboard/ProfitDashboard.js
+++ components/Home/ProfitDashboard/ProfitDashboard.js
@@ -1,83 +1,9 @@
-import React, { useEffect, useState, useContext } from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { StyleSheet, Image } from 'react-native';
-import { TouchableOpacity, Text, View, Pressable } from '@/components/Themed';
-import { useSQLiteContext } from 'expo-sqlite';
-import { UserContext } from '@/contexts/UserContext';
-import { getBetSlipResultsBetAmount, getBetSlipResultsWinnings } from '@/db/betslips/BetSlipsResults';
-import useTheme from '@/hooks/useTheme';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import BalanceChecker from '@/components/Home/ProfitDashboard/BalanceChecker/BalanceChecker';
-
-import { getBetSlipsLast7Days, getBetSlipsByBookieLast7Days } from '@/db/betslips/BetSlips';
-import { getWonBetSlipCountByBookieLast7Days, getProfitByBookieLast7Days } from '@/db/betslips/BetSlipsResults';
-import { getWonBetSlipCountLast7Days, getProfitLast7Days } from '@/db/betslips/BetSlipsResults';
+import BetSlipResults from './BetSlipResults';
 
 export default function ProfitDashboard({ openTransaction, openChooseBookie, transactions }) {
-
-    const { greenText, grayBackground, grayBorder, redText } = useTheme();
-    const { user, trigger, setTrigger, bookie, signedIn } = useContext(UserContext);
-
-    const db = useSQLiteContext();
-
-    const [betsPlaced, setBetsPlaced] = useState(0);
-    const [betsWon, setBetsWon] = useState(0);
-    const [profit, setProfit] = useState(0);
-
-    useEffect(() => {
-        if (!user) return;
-        if (!signedIn) return;
-        if (user.id === 0) {
-            setBetsPlaced(0);
-            setBetsWon(0);
-            setProfit(0);
-            return;
-        }
-        if (bookie.id === 0) {
-            // get Total Bets Placed for all bookies
-            getBetSlipsLast7Days(db, user.id).then((res) => {
-                setBetsPlaced(res);
-            });
-            // get Total Bets Won for all bookies
-            getWonBetSlipCountLast7Days(db, user.id).then((res) => {
-                setBetsWon(res);
-            });
-            // get Total Profit for all bookies
-            getProfitLast7Days(db, user.id).then((res) => {
-                setProfit(res);
-            });
-        } else {
-            getBetSlipsByBookieLast7Days(db, user.id, bookie.id).then((res) => {
-                setBetsPlaced(res);
-            });
-            getWonBetSlipCountByBookieLast7Days(db, user.id, bookie.id).then((res) => {
-                setBetsWon(res);
-            });
-            getProfitByBookieLast7Days(db, user.id, bookie.id).then((res) => {
-                setProfit(res);
-            });
-        }
-    }, [trigger, user, signedIn, bookie]);
-
-    const BetSlipResults = () => {
-        return (
-            <View style={styles.resultContainer}>
-                <View style={[styles.resultBox, { backgroundColor: grayBackground, borderWidth: 1, borderColor: grayBorder }]}>
-                    <Text>Bets Won</Text>
-                    <Text style={styles.moneyText}>{betsWon}</Text>
-                </View>
-                <View style={[styles.resultBox, { backgroundColor: grayBackground, borderWidth: 1, borderColor: grayBorder }]}>
-                    <Text>Bets Placed</Text>
-                    <Text style={styles.moneyText}>{betsPlaced}</Text>
-                </View>
-                <View style={[styles.resultBox, { backgroundColor: grayBackground, borderWidth: 1, borderColor: grayBorder }]}>
-                    <Text>Profit</Text>
-                    <Text style={[styles.moneyText, { color: profit > 0 ? greenText : redText }]}>
-                    {profit > 0 ? `+$${profit.toFixed(2)}` : `-$${Math.abs(profit).toFixed(2)}`}
-                    </Text>
-                </View>
-            </View>
-        );
-    }
 
     return (
     <>
