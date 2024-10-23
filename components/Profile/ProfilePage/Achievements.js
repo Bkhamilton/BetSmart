d2b36936f452ce1b5a8a6ab50cbbebff63f7739d
Bkhamilton <bkhamilton310@gmail.com>
2024-10-23 13:28:28 -0400

    moved achievements list to data folder

--- components/Profile/ProfilePage/Achievements.js
+++ components/Profile/ProfilePage/Achievements.js
@@ -1,74 +1,9 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { TouchableOpacity, Text, View, ScrollView } from '@/components/Themed';
-
-import Colors from '@/constants/Colors';
+import { Text, View, ScrollView } from '@/components/Themed';
+import { achievements } from '@/data/achievements';
 
 export default function Achievements() {
-
-    const achievements = [
-        {
-          id: 1,
-          name: '1 Bet Won',
-          description: 'You won your first bet!',
-          achieved: true,
-        },
-        {
-          id: 2,
-          name: '10 Bets Won',
-          description: 'You won 10 bets!',
-          achieved: true,
-        },
-        {
-          id: 3,
-          name: '100 Bets Won',
-          description: 'You won 100 bets!',
-          achieved: false,
-        },
-        {
-          id: 4,
-          name: '+200 Bet Won',
-          description: 'You won a bet with +200 odds!',
-          achieved: false,
-        },
-        {
-          id: 5,
-          name: '+500 Bet Won',
-          description: 'You won a bet with +500 odds!',
-          achieved: false,
-        },
-        {
-          id: 6,
-          name: '+1000 Bet Won',
-          description: 'You won a bet with +1000 odds!',
-          achieved: false,
-        },
-        {
-          id: 7,
-          name: '1 Bet Lost',
-          description: 'You lost your first bet!',
-          achieved: true,
-        },
-        {
-          id: 8,
-          name: '10 Bets Lost',
-          description: 'You lost 10 bets!',
-          achieved: true,
-        },
-        {
-          id: 9,
-          name: '100 Bets Lost',
-          description: 'You lost 100 bets!',
-          achieved: false,
-        },
-        {
-          id: 10,
-          name: 'Bet Streak',
-          description: 'You won 5 bets in a row!',
-          achieved: false,
-        },
-        // More achievements...
-    ];
 
     const AchievementComponent = ({ achievement }) => (
         <View style={[styles.achievementContainer, { borderWidth: achievement.achieved ? 4 : 1 }]}>
@@ -97,7 +32,7 @@ export default function Achievements() {
 
   const styles = StyleSheet.create({
     container: {
-      paddingHorizontal: 0,
+        paddingHorizontal: 0,
     },
     achievementContainer: {
         width: 50,
---
+++ data/achievements.js
@@ -0,0 +1,63 @@
+export const achievements = [
+    {
+      id: 1,
+      name: '1 Bet Won',
+      description: 'You won your first bet!',
+      achieved: true,
+    },
+    {
+      id: 2,
+      name: '10 Bets Won',
+      description: 'You won 10 bets!',
+      achieved: true,
+    },
+    {
+      id: 3,
+      name: '100 Bets Won',
+      description: 'You won 100 bets!',
+      achieved: false,
+    },
+    {
+      id: 4,
+      name: '+200 Bet Won',
+      description: 'You won a bet with +200 odds!',
+      achieved: false,
+    },
+    {
+      id: 5,
+      name: '+500 Bet Won',
+      description: 'You won a bet with +500 odds!',
+      achieved: false,
+    },
+    {
+      id: 6,
+      name: '+1000 Bet Won',
+      description: 'You won a bet with +1000 odds!',
+      achieved: false,
+    },
+    {
+      id: 7,
+      name: '1 Bet Lost',
+      description: 'You lost your first bet!',
+      achieved: true,
+    },
+    {
+      id: 8,
+      name: '10 Bets Lost',
+      description: 'You lost 10 bets!',
+      achieved: true,
+    },
+    {
+      id: 9,
+      name: '100 Bets Lost',
+      description: 'You lost 100 bets!',
+      achieved: false,
+    },
+    {
+      id: 10,
+      name: 'Bet Streak',
+      description: 'You won 5 bets in a row!',
+      achieved: false,
+    },
+    // More achievements...
+];
