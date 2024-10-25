d4efa54ed4c1edf807ff7acd06a59c77c7f18f9c
Bkhamilton <bkhamilton310@gmail.com>
2024-10-25 12:07:55 -0400

    Code Cleanup

--- components/PlaceBet/BetDetails/LeaguePropTypes/PropBanner/PropTypes/AltPlayer.js
+++ components/PlaceBet/BetDetails/LeaguePropTypes/PropBanner/PropTypes/AltPlayer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
-import { TouchableOpacity, Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
 import { AltPlayerComponent } from './ComponentTypes';
 
 export default function AltPlayer({ player, logo, stat }) {
