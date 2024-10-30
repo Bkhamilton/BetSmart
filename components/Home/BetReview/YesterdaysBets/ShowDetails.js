9c2511236bc11e810a9e3eefbc5343a70da86ba0
Bkhamilton <bkhamilton310@gmail.com>
2024-10-29 22:54:02 -0400

    updated to reflect change from betSlipRaw to betSlipResults in YesterdaysBets

--- components/Home/BetReview/YesterdaysBets/ShowDetails.js
+++ components/Home/BetReview/YesterdaysBets/ShowDetails.js
@@ -19,13 +19,13 @@ export default function ShowDetails({ betSlips, betsWon, totalBets }) {
             <ClearView style={[styles.spreadContainer, { paddingHorizontal: 12 }]}>
                 <ClearView style={[styles.detailsContainer, { flex: 0.3 }]}>
                     <Text style={{ fontSize: 38, fontWeight: '700' }}>{`${betsWon}/${totalBets}`}</Text>
-                    <Text style={{ fontSize: 18, fontWeight: '600', marginTop: 18 }}> bets</Text>
+                    <Text style={{ fontSize: 16, fontWeight: '600', marginTop: 20 }}> bets</Text>
                 </ClearView>
                 <ClearView style={[styles.detailsContainer, { marginBottom: 6, marginTop: 20, flex: 0.35, marginLeft: 16 }]}>
                     <Text style={{ fontSize: 16, fontWeight: '600' }}>Bet:</Text>
                     <ClearView>
                         {betSlips.map(bet => (
-                            <Text key={bet.id} style={{ fontSize: 18, fontWeight: bet.betStatus === 'won' ? '500' : '700', color: bet.betStatus === 'won' ? text : redText }}>{`$${bet.betAmount.toFixed(2)}`}</Text>
+                            <Text key={bet.id} style={{ fontSize: 18, fontWeight: bet.result === 1 ? '500' : '700', color: bet.result === 1 ? text : redText }}>{`$${bet.betAmount.toFixed(2)}`}</Text>
                         ))}
                     </ClearView>
                 </ClearView>
@@ -33,7 +33,7 @@ export default function ShowDetails({ betSlips, betsWon, totalBets }) {
                     <Text style={{ fontSize: 16, fontWeight: '600' }}>Won:</Text>
                     <ClearView>
                         {betSlips.map(bet => (
-                            <Text key={bet.id} style={{ fontSize: 18, fontWeight: bet.betStatus === 'won' ? '700' : '500', color: bet.betStatus === 'won' ? greenText : text }}>{`$${bet.betWon.toFixed(2)}`}</Text>
+                            <Text key={bet.id} style={{ fontSize: 18, fontWeight: bet.result === 1 ? '700' : '500', color: bet.result === 1 ? greenText : text }}>{`$${bet.winnings.toFixed(2)}`}</Text>
                         ))}
                     </ClearView>
                 </ClearView>
