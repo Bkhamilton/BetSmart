ab8360022cc5baeac6a60df009bb8d7f3ff6d7ce
Bkhamilton <bkhamilton310@gmail.com>
2024-10-29 20:36:44 -0400

    added getMonthlyWithdrawals, updated references to 'Withdrawal' to 'Withdraw'

--- db/user-specific/Transactions.js
+++ db/user-specific/Transactions.js
@@ -70,7 +70,7 @@ export const getWeeklyDeposits = async (db, userId) => {
 // Function to get total amount of money withdrawn in the last 7 days for a given user
 export const getWeeklyWithdrawals = async (db, userId) => {
     try {
-        const weeklyWithdrawals = await db.getAllAsync('SELECT SUM(amount) as total FROM Transactions WHERE userId = ? AND transactionType = "Withdrawal" AND timestamp >= datetime("now", "-7 days")', [userId]);
+        const weeklyWithdrawals = await db.getAllAsync('SELECT SUM(amount) as total FROM Transactions WHERE userId = ? AND transactionType = "Withdraw" AND timestamp >= datetime("now", "-7 days")', [userId]);
         return weeklyWithdrawals[0].total;
     } catch (error) {
         console.error('Error retrieving weekly withdrawals:', error);
@@ -85,6 +85,17 @@ export const getAllMonthlyDeposits = async (db, userId) => {
         return monthlyDeposits;
     } catch (error) {
         console.error('Error retrieving monthly deposits:', error);
+        throw error;
+    }
+};
+
+// Function to get all info related to withdrawals in the last month for a given user
+export const getAllMonthlyWithdrawals = async (db, userId) => {
+    try {
+        const monthlyWithdrawals = await db.getAllAsync('SELECT * FROM Transactions WHERE userId = ? AND transactionType = "Withdraw" AND timestamp >= datetime("now", "-30 days")', [userId]);
+        return monthlyWithdrawals;
+    } catch (error) {
+        console.error('Error retrieving monthly withdrawals:', error);
         throw error;
     }
 };
@@ -107,7 +118,7 @@ export const getMonthlyDeposits = async (db, userId) => {
 export const getMonthlyWithdrawals = async (db, userId) => {
     try {
         const monthlyWithdrawals = await db.getAllAsync(
-            'SELECT COALESCE(SUM(amount), 0) as total FROM Transactions WHERE userId = ? AND transactionType = "Withdrawal" AND timestamp >= datetime("now", "-30 days")',
+            'SELECT COALESCE(SUM(amount), 0) as total FROM Transactions WHERE userId = ? AND transactionType = "Withdraw" AND timestamp >= datetime("now", "-30 days")',
             [userId]
         );
         return monthlyWithdrawals[0].total;
