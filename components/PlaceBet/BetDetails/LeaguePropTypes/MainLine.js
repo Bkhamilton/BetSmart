e839d9c13e6336b195ebb8468e4e8c4186811a86
Bkhamilton <bkhamilton310@gmail.com>
2024-10-31 15:28:58 -0400

    updated references, added stat, and isOpen parameters to PropBanner

--- components/PlaceBet/BetDetails/LeaguePropTypes/MainLine.js
+++ components/PlaceBet/BetDetails/LeaguePropTypes/MainLine.js
@@ -1,32 +1,17 @@
 import React, { useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
-import { TouchableOpacity, Text, View } from '@/components/Themed';
-import { FontAwesome5 } from '@expo/vector-icons';
 import { BetContext } from '@/contexts/BetContext/BetContext';
-import useTheme from '@/hooks/useTheme';
 import PropBanner from './PropBanner/PropBanner';
 
 export default function MainLine({ info }) {
 
     const { league, currentGame } = useContext(BetContext);
 
-    const { iconColor } = useTheme();
-
-    const DisplayLine = ({ line }) => {
-        return (
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ borderWidth: 1, borderRadius: 20, width: 40, height: 40 }}/>
-                <Text>{line.name}</Text>
-                <Text>{line.value}</Text>
-            </View>
-        );
-    }
-
     const title = 'Main ' + info.propName + ' Lines';
 
     return (
         <>
-            <PropBanner title={title} type={"Player"}/>
+            <PropBanner title={title} stat={info.propName} type={"Player"} isOpen={true}/>
         </>
     );
 }
