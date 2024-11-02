3b21ac87e125d755f393ca840b55d209968bb25a
Bkhamilton <bkhamilton310@gmail.com>
2024-11-01 20:38:04 -0400

    added values to MainPlayer to track within ToRecordValueComponent

--- components/PlaceBet/BetDetails/LeaguePropTypes/PropBanner/PropTypes/MainPlayer.js
+++ components/PlaceBet/BetDetails/LeaguePropTypes/PropBanner/PropTypes/MainPlayer.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View, ClearView } from '@/components/Themed';
-import { MainPlayerComponent, ToRecordValueComponent } from './ComponentTypes';
+import { ToRecordValueComponent } from './ComponentTypes';
 import useTheme from '@/hooks/useTheme';
 
-export default function MainPlayer({ awayTeam, homeTeam }) {
+export default function MainPlayer({ stat, awayTeam, homeTeam }) {
 
     const { grayBackground } = useTheme();
 
     const onSelectProp = (player, value, odds) => {
-        console.log('Selected Prop:', player, value, odds);
+        console.log('Selected Prop:', stat, player, value, odds);
+    }
+
+    const pointValues = ['10', '15', '18', '20', '25', '30']
+
+    const assistValues = ['5', '6', '7', '8', '9', '10']
+
+    const reboundValues = ['8', '10', '12', '14', '16', '18']
+
+    const threesValues = ['1', '2', '3', '4']
+
+    const stealsValues = ['1', '2', '3', '4']
+
+    const getValues = (stat) => {
+        switch (stat) {
+            case 'Player Points':
+                return pointValues;
+            case 'Player Assists':
+                return assistValues;
+            case 'Player Rebounds':
+                return reboundValues;
+            case 'Player Threes':
+                return threesValues;
+            case 'Player Defense':
+                return stealsValues;
+            default:
+                return pointValues;
+        }
     }
 
     return (
@@ -23,8 +50,8 @@ export default function MainPlayer({ awayTeam, homeTeam }) {
                 </ClearView>
             </View>
             <View style={{ paddingHorizontal: 8 }}>
-                <ToRecordValueComponent value={4.5} team={awayTeam} odds={'-113'} select={onSelectProp}/>
-                <ToRecordValueComponent value={3.5} team={homeTeam} odds={'+108'} select={onSelectProp}/>
+                <ToRecordValueComponent team={awayTeam} odds={'-113'} values={getValues(stat)} select={onSelectProp}/>
+                <ToRecordValueComponent team={homeTeam} odds={'+108'} values={getValues(stat)} select={onSelectProp}/>
             </View>
         </View>
     )
