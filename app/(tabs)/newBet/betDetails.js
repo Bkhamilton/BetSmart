f7bfcc7f52760fec8a3d0216095d967be597ed25
Bkhamilton <bkhamilton310@gmail.com>
2024-11-02 21:03:37 -0400

    added ChoosePlayer modal to betDetails

--- app/(tabs)/newBet/betDetails.js
+++ app/(tabs)/newBet/betDetails.js
@@ -9,6 +9,7 @@ import useHookNewBet from '@/hooks/useHookNewBet';
 import useHookBetDetails from '@/hooks/useHookBetDetails';
 import BetSlipBanner from '@/components/PlaceBet/BetSlipBanner';
 import BetSlipModal from '@/components/Modals/BetSlipModal/BetSlipModal';
+import ChoosePlayer from '@/components/Modals/ChoosePlayer';
 import BetDetailsHeader from '@/components/PlaceBet/BetDetails/BetDetailsHeader';
 
 export default function BetDetailsScreen() {
@@ -42,6 +43,7 @@ export default function BetDetailsScreen() {
         close={closeChooseBookieModal}
         selectBookie={selectBookie}
       />
+      <ChoosePlayer/>
       {
         betSlip && (
           <BetSlipModal
