84d5d3c399b86f473690fbeb893eaae33b085a58
Bkhamilton <bkhamilton310@gmail.com>
2024-11-17 20:57:51 -0500

    added Skeleton functions to track the two different Mini-displays: Single Bet and Parlay

--- components/Profile/BetHistory/BetSlipDisplay/BetSlipDisplay.js
+++ components/Profile/BetHistory/BetSlipDisplay/BetSlipDisplay.js
@@ -3,6 +3,47 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
 export default function BetSlipDisplay({ betSlips, selectedType }) {
+
+    const SingleBetSlipDisplay = ({ betSlip }) => {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+    const ParlayBetSlipDisplay = ({ betSlip }) => {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+    const MiniBetSlipDisplay = ({ betSlip }) => {
+        return (
+            <View>
+                <View>
+                    <View>
+                        <Text>Bookie</Text>
+                    </View>
+                    <View>
+                        <Text>W</Text>
+                    </View>
+                </View>
+                <View>
+                    {/* Main Display for Bet Slip */}
+                    {/* If betSlip is a single bet Display A */}
+                    {/* If betSlip is a parlay bet Display B */}
+                </View>
+                <View>
+                    <Text>Bet: $4.00</Text>
+                    <Text>Won: $8.00</Text>
+                </View>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
             <View>
