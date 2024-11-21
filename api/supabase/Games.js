477d5d813c5becfd7eec177999f7632ee31449fc
Bkhamilton <bkhamilton310@gmail.com>
2024-11-20 23:52:14 -0500

    added Game retrieval functions for Supabase

---
+++ api/supabase/Games.js
@@ -0,0 +1,23 @@
+
+export const getUpcomingGames = async (supabase, seasonId) => {
+    const { data, error } = await supabase
+        .rpc('get_upcoming_games', { input_season_id: seasonId });
+    
+    if (error) {
+        throw error;
+    }
+    
+    return data;
+}
+
+export const getGamesByDateAndSeason = async (supabase, date, seasonId) => {
+    const { data, error } = await supabase
+        .rpc('get_games_by_date_and_season', { input_date: date, input_season_id: seasonId });
+    
+    if (error) {
+        throw error;
+    }
+    
+    return data;
+}
+    
