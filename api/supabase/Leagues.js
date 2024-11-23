0382c703e027e51722de24fffb1874dfacf98e16
Bkhamilton <bkhamilton310@gmail.com>
2024-11-22 22:11:39 -0500

    Add functions to retrieve league data from Supabase DB

---
+++ api/supabase/Leagues.js
@@ -0,0 +1,60 @@
+export const getAllLeagues = async (supabase) => {
+    try {
+        const { data, error } = await supabase
+            .from('Leagues')
+            .select('*')
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getActiveLeagues = async (supabase, date) => {
+    try {
+        const { data, error } = await supabase
+            .rpc('get_active_leagues', { p_date: date });
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getLeagueByName = async (supabase, leagueName) => {
+    try {
+        const { data, error } = await supabase
+            .from('Leagues')
+            .select('*')
+            .eq('leagueName', leagueName);
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getLeagueId = async (supabase, leagueName) => {
+    try {
+        const { data, error } = await supabase
+            .from('Leagues')
+            .select('id')
+            .eq('leagueName', leagueName);
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return data[0];
+    } catch (error) {
+        console.error(error);
+    }
+}
