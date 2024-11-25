8800ffe4e705318e43731f0a280790fc8e1ff18d
Bkhamilton <bkhamilton310@gmail.com>
2024-11-24 19:56:40 -0500

    created markets file to handle putting games and betmarkets into supabase

---
+++ api/supabase/markets.js
@@ -0,0 +1,233 @@
+import secrets from "@/secrets";
+import { getActiveLeagues } from "@/supabase/Leagues";
+import { getSeasonByDate } from "@/supabase/Seasons";
+import { getTeamId } from "@/supabase/Teams";
+import { insertGame, getGameByGameId } from "@/supabase/Games";
+import { insertBetTarget, getBetTargetsByGameId, getBetTargetIdByName } from "@/supabase/BetTargets";
+import { insertBetMarket, clearGameBetMarkets } from "@/supabase/BetMarkets";
+import { getBookieId } from "@/supabase/Bookies";
+import { getDateFull } from "@/utils/dateFunctions";
+// Handling array of objects
+/*
+Main object fields: 
+    id: string, 
+    sport_key: string, (api specific, 'basketball_nba, 'americanfootball_nfl', etc.)
+    sport_title: string, ('NBA', 'NFL', etc.)
+    commence_time: string, ('YYYY-MM-DDTHH:MM:SSZ')
+    home_team: string, 
+    away_team: string, 
+    bookmakers: array of Book objects
+
+Book object fields:
+    key: string, (Bookie name, 'draftkings', 'fanduel', etc.)
+    title: string, (Bookie name, 'DraftKings', 'FanDuel', etc.)
+    last_update: string, ('YYYY-MM-DDTHH:MM:SSZ')
+    markets: array of Market objects
+
+Market object fields:
+    key: string, (market name, 'spreads', 'totals', etc.)
+    last_update: string, ('YYYY-MM-DDTHH:MM:SSZ')
+    outcomes: array of Outcome objects
+
+Outcome object fields:
+    name: string, (for ML, Spread, teamName, for Totals, 'Over' or 'Under')
+    price: number, (American Odds)
+    point: number, (for Spread and Totals)
+*/
+
+/*
+Example market names:
+    h2h: Moneyline
+    spreads: Spread
+    totals: Total Over/Under
+*/
+
+/*
+Mapping of league names to API-specific sport keys:
+MUST HAVES
+    NBA: 'basketball_nba'
+    NFL: 'americanfootball_nfl'
+    MLB: 'baseball_mlb'
+    NHL: 'icehockey_nhl'
+ADD LATER
+    NCAAF: 'americanfootball_ncaaf'
+    NCAAB: 'basketball_ncaab'
+    WNBA: 'basketball_wnba'
+    MMA: 'mma_mixed_martial_arts'
+    MLS: 'soccer_usa_mls'
+    EPL: 'soccer_epl'
+*/
+
+/*
+DB Important Info:
+    Insert BetMarket => (db, gameId, marketType, timestamp, value, odds, overUnder, betTargetId, bookieId)
+*/
+
+const bookieMapping = {
+    'DraftKings': 'draftkings',
+    'FanDuel': 'fanduel',
+    'BetMGM': 'betmgm',
+    'Caesars': 'williamhill_us',
+}
+
+const leagueMapping = {
+    'NBA': 'basketball_nba',
+    'NFL': 'americanfootball_nfl',
+    'MLB': 'baseball_mlb',
+    'NHL': 'icehockey_nhl'
+}
+
+export const getMarkets = async (db, league, markets) => {
+    try {
+        const marketString = markets.join(',');
+        const response = await fetch(`https://api.the-odds-api.com/v4/sports/${league}/odds/?apiKey=${secrets.THE_ODDS_API_KEY}&regions=us&markets=${marketString}&oddsFormat=american`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const getBig3Markets = async (db, league) => {
+    try {
+        const markets = ['h2h', 'spreads', 'totals'];
+        const data = await getMarkets(db, league, markets);
+        return data;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const getMarketType = (market) => {
+    switch (market) {
+        case 'h2h':
+            return 'moneyline';
+        case 'spreads':
+            return 'spread';
+        case 'totals':
+            return 'totals';
+        default:
+            return '';
+    }
+}
+
+const getOdds = (odds) => {
+    if (odds > 0) {
+      return '+' + odds;
+    } else {
+      return odds.toString();
+    }
+}
+
+const addOutcomeToDB = async (db, outcome, last_update, market, bookId, gameId, betTarget) => {
+    try {
+        const { name, price, point = null } = outcome;
+        // Insert BetMarket => (db, gameId, marketType, timestamp, value, odds, overUnder, betTargetId, bookieId)
+        const marketType = getMarketType(market);
+        const overUnder = name === 'Over' || name === 'Under' ? name : '';
+        const value = market === 'h2h' ? name : point;
+        const betTargetId = name === 'Over' || name === 'Under' ? betTarget : await getBetTargetIdByName(db, name);
+        const odds = getOdds(price);
+        if (!betTargetId) {
+            console.log('No betTargetId found for ' + name);
+            return;
+        }
+        await insertBetMarket(db, gameId, marketType, last_update, value, odds, overUnder, betTargetId, bookId);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const handleMarketData = async (db, market, bookieId, gameId, betTargetId) => {
+    try {
+        const { key, last_update, outcomes } = market;
+        for (let outcome of outcomes) {
+            await addOutcomeToDB(db, outcome, last_update, key, bookieId, gameId, betTargetId);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const handleBookieData = async (db, book, gameId, betTargetId) => {
+    try {
+        const { key, title, last_update, markets } = book;
+        const bookieId = await getBookieId(db, title);
+        for (let market of markets) {
+            await handleMarketData(db, market, bookieId, gameId, betTargetId);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const handleGameData = async (db, game, league) => {
+    try {
+        // Check if game is already in DB
+        // If it is, don't add it again
+        const curGame = await getGameByGameId(db, game.id);
+        if (!curGame) {
+            const date = getDateFull(game.commence_time);
+            const curSeason = await getSeasonByDate(db, league.id, date);
+            const homeTeam = await getTeamId(db, game.home_team);
+            if (!homeTeam) {
+                console.log('No homeTeamId found for ' + game.home_team);
+                return;
+            }
+            const homeTeamId = homeTeam.id;
+            const awayTeam = await getTeamId(db, game.away_team);
+            if (!awayTeam) {
+                console.log('No awayTeamId found for ' + game.away_team);
+                return;
+            }
+            const awayTeamId = awayTeam.id;
+            await insertGame(db, game.id, curSeason.id, date, game.commence_time, homeTeamId, awayTeamId);
+        }
+        const betTarget = await getBetTargetsByGameId(db, game.id);
+        if (betTarget.length === 0) {
+            await clearGameBetMarkets(db, game.id);
+        }
+        const betTargetId = await insertBetTarget(db, 'Game', `${game.away_team} vs ${game.home_team}`, null, game.id);
+        for (let book of game.bookmakers) {
+            if (!bookieMapping[book.title]) continue;
+            await handleBookieData(db, book, game.id, betTargetId);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const refreshBettingMarkets = async (db, league) => {
+    try {
+        const leagueName = leagueMapping[league.leagueName];
+        const data = await getBig3Markets(db, leagueName);
+        for (let game of data) {
+            await handleGameData(db, game, league);
+        }
+        console.log('Refreshed betting markets for ' + league.leagueName);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }    
+}
+
+export const refreshBettingMarketsLeagues = async (db, leagues) => {
+    try {
+        for (let league of leagues) {
+            await refreshBettingMarkets(db, league);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const refreshAllBettingMarkets = async (db) => {
+    try {
+        const leagues = await getActiveLeagues(db);
+        for (let league of leagues) {
+            await refreshBettingMarkets(db, league);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
