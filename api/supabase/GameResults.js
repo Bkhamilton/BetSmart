b2cde92a2c71c0bc93996dcfca8823ecf3f0f05f
Bkhamilton <bkhamilton310@gmail.com>
2024-12-04 20:16:33 -0500

    added function to get gameresults for a given gameId

--- api/supabase/GameResults.js
+++ api/supabase/GameResults.js
@@ -1,3 +1,4 @@
+// Function to get all relevant game results
 export const getAllRelevantGameResults = async (supabase) => {
     const { data, error } = await supabase
         .rpc('get_all_relevant_game_results');
@@ -9,6 +10,21 @@ export const getAllRelevantGameResults = async (supabase) => {
     return data;
 }
 
+// Function to get game results for a game
+export const getGameResultsForGame = async (supabase, gameId) => {
+    const { data, error } = await supabase
+        .from('GameResults')
+        .select('*')
+        .eq('gameId', gameId);
+
+    if (error) {
+        throw error;
+    }
+
+    return data;
+}
+
+// Function to insert a game result
 export const insertGameResult = async (supabase, gameId, homeScore, awayScore, winner) => {
     const { data, error } = await supabase
         .from('GameResults')
