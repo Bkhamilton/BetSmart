16f24349373e7e87218262c5aca6898ccf0484b4
Bkhamilton <bkhamilton310@gmail.com>
2024-12-05 17:01:03 -0500

    created weeklyBets data array to help with WeeklyBetReview

---
+++ data/weeklyBets.js
@@ -0,0 +1,272 @@
+export const weeklyBets = [
+    {
+        "id": 105,
+        "formatId": 2,
+        "date": "2024-11-21T21:07:18.441Z",
+        "odds": "+188",
+        "betAmount": 16,
+        "winnings": 46.08,
+        "userId": 1,
+        "bookieId": 1,
+        "result": 0,
+        "bets": [
+            {
+                "id": 160,
+                "betSlipId": 105,
+                "gameId": "a94279dcfb065a9c40cc6869d6b68fcd",
+                "odds": "+120",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T00:10:00Z",
+                "homeTeamName": "Charlotte Hornets",
+                "homeTeamAbv": "CHA",
+                "awayTeamName": "Detroit Pistons",
+                "awayTeamAbv": "DET",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 162,
+                        "participantBetId": 160,
+                        "betMarketId": 126294,
+                        "marketType": "moneyline",
+                        "value": "Charlotte Hornets",
+                        "odds": "+120",
+                        "overUnder": "",
+                        "betTargetId": 7,
+                        "betTarget": "Charlotte Hornets",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            },
+            {
+                "id": 159,
+                "betSlipId": 105,
+                "gameId": "fc0af137075bbfab01411d88dcec50a5",
+                "odds": "-325",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T00:40:00Z",
+                "homeTeamName": "Toronto Raptors",
+                "homeTeamAbv": "TOR",
+                "awayTeamName": "Minnesota Timberwolves",
+                "awayTeamAbv": "MIN",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 161,
+                        "participantBetId": 159,
+                        "betMarketId": 126318,
+                        "marketType": "moneyline",
+                        "value": "Minnesota Timberwolves",
+                        "odds": "-325",
+                        "overUnder": "",
+                        "betTargetId": 21,
+                        "betTarget": "Minnesota Timberwolves",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": 106,
+        "formatId": 5,
+        "date": "2024-11-21T23:18:36.954Z",
+        "odds": "+2735",
+        "betAmount": 5,
+        "winnings": 141.75,
+        "userId": 1,
+        "bookieId": 1,
+        "result": 0,
+        "bets": [
+            {
+                "id": 161,
+                "betSlipId": 106,
+                "gameId": "5c89c0aa80d567680f352f9c2a3b8222",
+                "odds": "+258",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T03:40:00Z",
+                "homeTeamName": "Los Angeles Lakers",
+                "homeTeamAbv": "LAL",
+                "awayTeamName": "Orlando Magic",
+                "awayTeamAbv": "ORL",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 164,
+                        "participantBetId": 161,
+                        "betMarketId": 126368,
+                        "marketType": "spread",
+                        "value": -4.5,
+                        "odds": "-112",
+                        "overUnder": "",
+                        "betTargetId": 17,
+                        "betTarget": "Los Angeles Lakers",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    },
+                    {
+                        "legId": 163,
+                        "participantBetId": 161,
+                        "betMarketId": 126371,
+                        "marketType": "totals",
+                        "value": 216.5,
+                        "odds": "-112",
+                        "overUnder": "Under",
+                        "betTargetId": 4340,
+                        "betTarget": "Orlando Magic vs Los Angeles Lakers",
+                        "targetType": "Game",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            },
+            {
+                "id": 162,
+                "betSlipId": 106,
+                "gameId": "882bf20a823fe5d98b78592b3e2ccc89",
+                "odds": "+120",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T01:10:00Z",
+                "homeTeamName": "San Antonio Spurs",
+                "homeTeamAbv": "SAS",
+                "awayTeamName": "Utah Jazz",
+                "awayTeamAbv": "UTA",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 165,
+                        "participantBetId": 162,
+                        "betMarketId": 126337,
+                        "marketType": "moneyline",
+                        "value": "Utah Jazz",
+                        "odds": "+120",
+                        "overUnder": "",
+                        "betTargetId": 29,
+                        "betTarget": "Utah Jazz",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            },
+            {
+                "id": 163,
+                "betSlipId": 106,
+                "gameId": "fc0af137075bbfab01411d88dcec50a5",
+                "odds": "+260",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T00:40:00Z",
+                "homeTeamName": "Toronto Raptors",
+                "homeTeamAbv": "TOR",
+                "awayTeamName": "Minnesota Timberwolves",
+                "awayTeamAbv": "MIN",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 166,
+                        "participantBetId": 163,
+                        "betMarketId": 126319,
+                        "marketType": "moneyline",
+                        "value": "Toronto Raptors",
+                        "odds": "+260",
+                        "overUnder": "",
+                        "betTargetId": 6,
+                        "betTarget": "Toronto Raptors",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": 107,
+        "formatId": 2,
+        "date": "2024-11-22T04:10:50.487Z",
+        "odds": "+692",
+        "betAmount": 5,
+        "winnings": 39.6,
+        "userId": 1,
+        "bookieId": 1,
+        "result": 1,
+        "bets": [
+            {
+                "id": 164,
+                "betSlipId": 107,
+                "gameId": "a94279dcfb065a9c40cc6869d6b68fcd",
+                "odds": "+120",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T00:10:00Z",
+                "homeTeamName": "Charlotte Hornets",
+                "homeTeamAbv": "CHA",
+                "awayTeamName": "Detroit Pistons",
+                "awayTeamAbv": "DET",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 167,
+                        "participantBetId": 164,
+                        "betMarketId": 126294,
+                        "marketType": "moneyline",
+                        "value": "Charlotte Hornets",
+                        "odds": "+120",
+                        "overUnder": "",
+                        "betTargetId": 7,
+                        "betTarget": "Charlotte Hornets",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            },
+            {
+                "id": 165,
+                "betSlipId": 107,
+                "gameId": "fc0af137075bbfab01411d88dcec50a5",
+                "odds": "+260",
+                "seasonId": 9,
+                "date": "2024-11-21",
+                "timestamp": "2024-11-22T00:40:00Z",
+                "homeTeamName": "Toronto Raptors",
+                "homeTeamAbv": "TOR",
+                "awayTeamName": "Minnesota Timberwolves",
+                "awayTeamAbv": "MIN",
+                "leagueId": 1,
+                "league": "NBA",
+                "legs": [
+                    {
+                        "legId": 168,
+                        "participantBetId": 165,
+                        "betMarketId": 126319,
+                        "marketType": "moneyline",
+                        "value": "Toronto Raptors",
+                        "odds": "+260",
+                        "overUnder": "",
+                        "betTargetId": 6,
+                        "betTarget": "Toronto Raptors",
+                        "targetType": "Team",
+                        "betTypeId": 1,
+                        "betType": "Main"
+                    }
+                ]
+            }
+        ]
+    }
+];
