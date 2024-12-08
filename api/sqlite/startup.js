2cb43cb6a3f7d8ee38900a982c424de6d42a578f
Bkhamilton <bkhamilton310@gmail.com>
2024-12-07 21:33:16 -0500

    removed comments for initalizing database function

--- api/sqlite/startup.js
+++ api/sqlite/startup.js
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import secrets from '@/secrets';
 import { createTables } from '@/api/sqlite/createTables';
 import { getActiveLeagueNames } from '@/db/general/Leagues';
+import { syncInitialData, checkForUpdates } from '@/api/supabase/startup';
 
 // Set up Supabase client
 const supabaseUrl = secrets.SUPABASE_URL;
@@ -22,59 +23,57 @@ const supabase = createClient(
 );
 
 export const initializeDatabase = async (db) => {
-  try {
-    const isFirstLaunch = await AsyncStorage.getItem('isFirstLaunch');
-    if (isFirstLaunch === null) {
-        // First-time initialization
-        await AsyncStorage.setItem('isFirstLaunch', 'false');
-        console.log('First-time initialization');
-        /*
-        await setupDatabase(db);
-        await syncInitialData(db, supabase);
-        */
-    } else {
-        console.log('Subsequent loads');
-        // Subsequent loads
-        // await checkForUpdates(db, supabase);
-    }
-  } catch (error) {
-    console.error('Error initializing database:', error.message);
-  }  
+    try {
+        const isFirstLaunch = await AsyncStorage.getItem('isFirstLaunch');
+        if (isFirstLaunch === null) {
+            // First-time initialization
+            await AsyncStorage.setItem('isFirstLaunch', 'false');
+            console.log('First-time initialization');
+            await setupDatabase(db);
+            await syncInitialData(db, supabase);
+        } else {
+            console.log('Subsequent loads');
+            // Subsequent loads
+            await checkForUpdates(db, supabase);
+        }
+    } catch (error) {
+        console.error('Error initializing database:', error.message);
+    }  
 };
 
 export const setupDatabase = async (db) => {
-  await createTables(db);
+    await createTables(db);
 }
 
 // Function to drop the tables
 export const dropTables = async (db) => {
-  await db.execAsync(`
-    DROP TABLE IF EXISTS Bookies;
-    DROP TABLE IF EXISTS Leagues;
-    DROP TABLE IF EXISTS Teams;
-    DROP TABLE IF EXISTS Seasons;
-    DROP TABLE IF EXISTS Games;
-    DROP TABLE IF EXISTS Players;
-    DROP TABLE IF EXISTS Users;
-    DROP TABLE IF EXISTS UserSessions;
-    DROP TABLE IF EXISTS Balance;
-    DROP TABLE IF EXISTS Transactions;
-    DROP TABLE IF EXISTS Bonuses;
-    DROP TABLE IF EXISTS LeagueProps;
-    DROP TABLE IF EXISTS LeaguePropsInfo;
-    DROP TABLE IF EXISTS BetTargets;
-    DROP TABLE IF EXISTS BetTypes;
-    DROP TABLE IF EXISTS BetFormats;
-    DROP TABLE IF EXISTS BetSlips;
-    DROP TABLE IF EXISTS ParticipantBets;
-    DROP TABLE IF EXISTS Legs;
-    DROP TABLE IF EXISTS BetSlipsResults;
-    DROP TABLE IF EXISTS ParticipantBetsResults;
-    DROP TABLE IF EXISTS LegsResults;
-    DROP TABLE IF EXISTS BetMarkets;
-    DROP TABLE IF EXISTS FetchHistory;
-    DROP TABLE IF EXISTS MarketFetchHistory;
-    DROP TABLE IF EXISTS GameResults;
-  `);
-  console.log('Tables dropped');
+    await db.execAsync(`
+        DROP TABLE IF EXISTS Bookies;
+        DROP TABLE IF EXISTS Leagues;
+        DROP TABLE IF EXISTS Teams;
+        DROP TABLE IF EXISTS Seasons;
+        DROP TABLE IF EXISTS Games;
+        DROP TABLE IF EXISTS Players;
+        DROP TABLE IF EXISTS Users;
+        DROP TABLE IF EXISTS UserSessions;
+        DROP TABLE IF EXISTS Balance;
+        DROP TABLE IF EXISTS Transactions;
+        DROP TABLE IF EXISTS Bonuses;
+        DROP TABLE IF EXISTS LeagueProps;
+        DROP TABLE IF EXISTS LeaguePropsInfo;
+        DROP TABLE IF EXISTS BetTargets;
+        DROP TABLE IF EXISTS BetTypes;
+        DROP TABLE IF EXISTS BetFormats;
+        DROP TABLE IF EXISTS BetSlips;
+        DROP TABLE IF EXISTS ParticipantBets;
+        DROP TABLE IF EXISTS Legs;
+        DROP TABLE IF EXISTS BetSlipsResults;
+        DROP TABLE IF EXISTS ParticipantBetsResults;
+        DROP TABLE IF EXISTS LegsResults;
+        DROP TABLE IF EXISTS BetMarkets;
+        DROP TABLE IF EXISTS FetchHistory;
+        DROP TABLE IF EXISTS MarketFetchHistory;
+        DROP TABLE IF EXISTS GameResults;
+    `);
+    console.log('Tables dropped');
 };
