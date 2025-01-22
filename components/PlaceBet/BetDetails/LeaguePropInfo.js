aba6a336d67f3e6cd1ead2d8e865c863f872dad0
Bkhamilton <bkhamilton310@gmail.com>
2025-01-21 19:49:14 -0500

    Add fallback rendering for missing components in LeaguePropInfo

--- components/PlaceBet/BetDetails/LeaguePropInfo.js
+++ components/PlaceBet/BetDetails/LeaguePropInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { ClearView } from '@/components/Themed';
+import { ClearView, Text } from '@/components/Themed';
 import componentMapping from '@/utils/componentMapping';
 
 export default function LeaguePropInfo({ leaguePropInfo }) {
@@ -8,6 +8,7 @@ export default function LeaguePropInfo({ leaguePropInfo }) {
         <>
             {leaguePropInfo.map((info, index) => {
                 const Component = componentMapping[info.propValue];
+                if (!Component) return <Text key={index}>{info.propValue}</Text>;
                 return (
                     <ClearView key={index} style={styles.container}>
                         <Component info={info}/>
