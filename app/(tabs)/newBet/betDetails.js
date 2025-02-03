0676d0ebdaaefc7034dfb602190f719981c319c3
Bkhamilton <bkhamilton310@gmail.com>
2025-02-02 23:27:36 -0500

    removed unused parameter from LeaguePropInfo

--- app/(tabs)/newBet/betDetails.js
+++ app/(tabs)/newBet/betDetails.js
@@ -70,7 +70,6 @@ export default function BetDetailsScreen() {
                             curLeagueProp={curLeagueProp}
                         />
                         <LeaguePropInfo 
-                            leagueProp={curLeagueProp}
                             leaguePropInfo={leaguePropInfo}
                         />
                     </>
