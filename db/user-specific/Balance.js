f4380280b7304a830d72c433045de80f7314dc36
Bkhamilton <bkhamilton310@gmail.com>
2025-02-04 14:07:14 -0500

    improved spacing for code cleanup

--- db/user-specific/Balance.js
+++ db/user-specific/Balance.js
@@ -2,85 +2,85 @@ import * as SQLite from 'expo-sqlite';
 
 // Function to get a user's balance
 export const getBalance = async (db, userId) => {
-  try {
-    const allRows = await db.getAllAsync('SELECT * FROM Balance WHERE userID = ?', [userId]);
-    return allRows;
-  } catch (error) {
-    console.error('Error getting balance:', error);
-    throw error;
-  }
+    try {
+        const allRows = await db.getAllAsync('SELECT * FROM Balance WHERE userID = ?', [userId]);
+        return allRows;
+    } catch (error) {
+        console.error('Error getting balance:', error);
+        throw error;
+    }
 };
 
 // Function to get a user's balance
 export const getBalanceByUser = async (db, userId) => {
-  try {
-    const allRows = await db.getAllAsync('SELECT Balance.*, Bookies.name as bookieName FROM Balance JOIN Bookies ON Balance.bookieId = Bookies.id WHERE Balance.userID = ?', [userId]);
-    return allRows;
-  } catch (error) {
-    console.error('Error getting balance:', error);
-    throw error;
-  }
+    try {
+        const allRows = await db.getAllAsync('SELECT Balance.*, Bookies.name as bookieName FROM Balance JOIN Bookies ON Balance.bookieId = Bookies.id WHERE Balance.userID = ?', [userId]);
+        return allRows;
+    } catch (error) {
+        console.error('Error getting balance:', error);
+        throw error;
+    }
 };
 
 // Function to get all bookie names with balance
 export const getBookieNamesWithBalance = async (db, userId) => {
-  try {
-    const allRows = await db.getAllAsync('SELECT Bookies.name FROM Balance JOIN Bookies ON Balance.bookieId = Bookies.id WHERE Balance.userID = ?', [userId]);
-    return allRows.map((row) => row.name);
-  } catch (error) {
-    console.error('Error getting bookie names with balance:', error);
-    throw error;
-  }
+    try {
+        const allRows = await db.getAllAsync('SELECT Bookies.name FROM Balance JOIN Bookies ON Balance.bookieId = Bookies.id WHERE Balance.userID = ?', [userId]);
+        return allRows.map((row) => row.name);
+    } catch (error) {
+        console.error('Error getting bookie names with balance:', error);
+        throw error;
+    }
 };
 
 // Function to get all bookie names and ids with balance
 export const getValidBookies = async (db, userId) => {
-  try {
-    const allRows = await db.getAllAsync('SELECT Balance.bookieId, Bookies.name FROM Balance JOIN Bookies ON Balance.bookieId = Bookies.id WHERE Balance.userID = ?', [userId]);
-    return allRows;
-  } catch (error) {
-    console.error('Error getting bookie names with balance:', error);
-    throw error;
-  }
+    try {
+        const allRows = await db.getAllAsync('SELECT Balance.bookieId, Bookies.name FROM Balance JOIN Bookies ON Balance.bookieId = Bookies.id WHERE Balance.userID = ?', [userId]);
+        return allRows;
+    } catch (error) {
+        console.error('Error getting bookie names with balance:', error);
+        throw error;
+    }
 }
 
 // Function to insert a balance
 export const insertBalance = async (db, bookieId, balance, userId) => {
-  try {
-    const result = await db.runAsync('INSERT INTO Balance (bookieId, balance, userID) VALUES (?, ?, ?)', [bookieId, balance, userId]);
-    return result.lastInsertRowId;
-  } catch (error) {
-    console.error('Error inserting balance:', error);
-    throw error;
-  }
+    try {
+        const result = await db.runAsync('INSERT INTO Balance (bookieId, balance, userID) VALUES (?, ?, ?)', [bookieId, balance, userId]);
+        return result.lastInsertRowId;
+    } catch (error) {
+        console.error('Error inserting balance:', error);
+        throw error;
+    }
 };
 
 // Function to update a balance
 export const updateBalance = async (db, bookieId, balance, userId) => {
-  try {
-    await db.runAsync('UPDATE Balance SET balance = ? WHERE bookieId = ? AND userID = ?', [balance, bookieId, userId]);
-  } catch (error) {
-    console.error('Error updating balance:', error);
-    throw error;
-  }
+    try {
+        await db.runAsync('UPDATE Balance SET balance = ? WHERE bookieId = ? AND userID = ?', [balance, bookieId, userId]);
+    } catch (error) {
+        console.error('Error updating balance:', error);
+        throw error;
+    }
 };
 
 // Function to update a balance by user
 export const updateUserBalance  = async (db, bookieId, toAdd, userId) => {
-  try {
-    await db.runAsync('UPDATE Balance SET balance = balance + ? WHERE bookieId = ? AND userID = ?', [toAdd, bookieId, userId]);
-  } catch (error) {
-    console.error('Error updating balance:', error);
-    throw error;
-  }
+    try {
+        await db.runAsync('UPDATE Balance SET balance = balance + ? WHERE bookieId = ? AND userID = ?', [toAdd, bookieId, userId]);
+    } catch (error) {
+        console.error('Error updating balance:', error);
+        throw error;
+    }
 }
 
 // Function to delete a balance
 export const deleteBalance = async (db, bookieId, userId) => {
-  try {
-    await db.runAsync('DELETE FROM Balance WHERE bookieId = ? AND UserID = ?', [bookieId, userId]);
-  } catch (error) {
-    console.error('Error deleting balance:', error);
-    throw error;
-  }
+    try {
+        await db.runAsync('DELETE FROM Balance WHERE bookieId = ? AND UserID = ?', [bookieId, userId]);
+    } catch (error) {
+        console.error('Error deleting balance:', error);
+        throw error;
+    }
 };
--- db/user-specific/Users.js
+++ db/user-specific/Users.js
@@ -2,64 +2,64 @@ import * as SQLite from 'expo-sqlite';
 
 // Function to get all users
 export const getAllUsers = async (db) => {
-  try {
-    const allRows = await db.getAllAsync('SELECT * FROM Users');
-    return allRows;
-  } catch (error) {
-    console.error('Error getting all users:', error);
-    throw error;
-  }
+    try {
+        const allRows = await db.getAllAsync('SELECT * FROM Users');
+        return allRows;
+    } catch (error) {
+        console.error('Error getting all users:', error);
+        throw error;
+    }
 };
 
 // Function to get a user by uysername and password
 export const getUser = async (db, username, password) => {
-  try {
-    const user = await db.getAllAsync('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password]);
-    return user[0];
-  } catch (error) {
-    console.error('Error getting user:', error);
-    throw error;
-  }
+    try {
+        const user = await db.getAllAsync('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password]);
+        return user[0];
+    } catch (error) {
+        console.error('Error getting user:', error);
+        throw error;
+    }
 };
 
 // Function to get a user by ID
 export const getUserById = async (db, id) => {
-  try {
-    const user = await db.getAllAsync('SELECT * FROM Users WHERE id = ?', [id]);
-    return user[0];
-  } catch (error) {
-    console.error('Error getting user:', error);
-    throw error;
-  }
+    try {
+        const user = await db.getAllAsync('SELECT * FROM Users WHERE id = ?', [id]);
+        return user[0];
+    } catch (error) {
+        console.error('Error getting user:', error);
+        throw error;
+    }
 };
 
 // Function to insert a user
 export const insertUser = async (db, name, email, username, password) => {
-  try {
-    const result = await db.runAsync('INSERT INTO Users (name, email, username, password) VALUES (?, ?, ?, ?)', [name, email, username, password]);
-    return result.lastInsertRowId;
-  } catch (error) {
-    console.error('Error inserting user:', error);
-    throw error;
-  }
+    try {
+        const result = await db.runAsync('INSERT INTO Users (name, email, username, password) VALUES (?, ?, ?, ?)', [name, email, username, password]);
+        return result.lastInsertRowId;
+    } catch (error) {
+        console.error('Error inserting user:', error);
+        throw error;
+    }
 };
 
 // Function to update a user
 export const updateUser = async (db, id, name, email, username, password) => {
-  try {
-    await db.runAsync('UPDATE users SET name = ?, username = ?, email = ?, password = ? WHERE id = ?', [name, email, username, password, id]);
-  } catch (error) {
-    console.error('Error updating user:', error);
-    throw error;
-  }
+    try {
+        await db.runAsync('UPDATE users SET name = ?, username = ?, email = ?, password = ? WHERE id = ?', [name, email, username, password, id]);
+    } catch (error) {
+        console.error('Error updating user:', error);
+        throw error;
+    }
 };
 
 // Function to delete a user
 export const deleteUser = async (db, id) => {
-  try {
-    await db.runAsync('DELETE FROM users WHERE id = ?', [id]);
-  } catch (error) {
-    console.error('Error deleting user:', error);
-    throw error;
-  }
+    try {
+        await db.runAsync('DELETE FROM users WHERE id = ?', [id]);
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
 };
