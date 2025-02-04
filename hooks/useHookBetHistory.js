088cae5cde010a4960fe1b46c28446066c4e3df1
Bkhamilton <bkhamilton310@gmail.com>
2025-02-03 22:57:00 -0500

    refactor: separate fetching of open and settled bets in useHookBetHistory

--- hooks/useHookBetHistory.js
+++ hooks/useHookBetHistory.js
@@ -2,7 +2,8 @@ import { useState, useEffect, useContext } from 'react';
 import { DBContext } from '@/contexts/DBContext';
 import { UserContext } from '@/contexts/UserContext';
 import { getOpenBetSlips } from '@/db/betslips/BetSlips';
-import { fillBetSlips } from '@/contexts/BetContext/betSlipHelpers';
+import { getSettledBetSlips } from '@/db/betslips/BetSlipsResults';
+import { fillBetSlips, fillBetSlipsWithResults } from '@/contexts/BetContext/betSlipHelpers';
 
 const useHookBetHistory = () => {
 
@@ -18,19 +19,33 @@ const useHookBetHistory = () => {
         setSelectedType(type);
     }
 
+    const fetchOpenbets = async () => {
+        try {
+            const betSlips = await getOpenBetSlips(db, user.id);
+            const betSlipsWithBets = await fillBetSlips(db, betSlips);
+            setBetSlips(betSlipsWithBets);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const fetchSettledBets = async () => {
+        try {
+            const betSlips = await getSettledBetSlips(db, user.id);
+            const betSlipsWithBets = await fillBetSlipsWithResults(db, betSlips);
+            setBetSlips(betSlipsWithBets);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const betSlips = await getOpenBetSlips(db, user.id);
-                const betSlipsWithBets = await fillBetSlips(db, betSlips);
-                setBetSlips(betSlipsWithBets);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        
-        fetchData();
-    }, []);
+        if (selectedType === 'Open') {
+            fetchOpenbets();
+        } else {
+            fetchSettledBets();
+        }
+    }, [selectedType]);
 
     return {
         selectedType,
