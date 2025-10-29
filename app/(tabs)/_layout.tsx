1bbf27d017d1b6b6756f608ca864c26b730a3cfc
Bkhamilton <bkhamilton310@gmail.com>
2025-10-28 21:08:05 -0400

    Add href properties to Tab options for improved navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -58,6 +58,7 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: 'BetSmart',
+                    href: '/(tabs)/',
                     tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
                     tabBarLabel: 'Home',
                     headerShown: false,
@@ -67,6 +68,7 @@ export default function TabLayout() {
                 name="insights"
                 options={{
                     title: 'Insights',
+                    href: '/(tabs)/insights',
                     tabBarIcon: ({ color }) => <TabBarIcon name="eye" color={color} />,
                     tabBarLabel: 'Insights',
                     headerShown: false,
@@ -92,6 +94,7 @@ export default function TabLayout() {
                 name="data"
                 options={{
                     title: 'Data',
+                    href: '/(tabs)/data',
                     tabBarIcon: ({ color }) => <TabBarIcon name="pie-chart" color={color} />,
                     tabBarLabel: 'Data',
                     headerShown: false,
@@ -101,6 +104,7 @@ export default function TabLayout() {
                 name="profile"
                 options={{
                     title: 'Profile',
+                    href: '/(tabs)/profile/profilePage',
                     tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
                     headerShown: false,
                 }}
